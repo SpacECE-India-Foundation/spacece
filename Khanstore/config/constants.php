617c444457f32b2972ad3c342fc27ed5fb6da1f7
Krishna <krishna.thorat20@vit.edu>
2021-11-15 16:48:42 +0530

    db updated

--- Khanstore/config/constants.php
+++ Khanstore/config/constants.php
@@ -1,8 +1,8 @@
 <?php
 
-define('HOST', 'localhost');
-define('USER', 'gamention');
-define('PASSWORD', 'aa123456');
+define('HOST', '3.109.14.4');
+define('USER', 'ostechnix');
+define('PASSWORD', 'Password123#@!');
 define('DATABASE_NAME', 'khanstore');
 
 define('CURRENCY', '₹');
