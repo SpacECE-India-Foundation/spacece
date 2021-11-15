58fa124210e74a552ad82514a07ca37b2930ede9
Krishna <krishna.thorat20@vit.edu>
2021-11-15 13:33:39 +0530

    modified index.html to index.php and added __DIR__

---
+++ index.php
@@ -0,0 +1,470 @@
+<!DOCTYPE html>
+<!-- Version SpacECE-31 -->
+<html lang="en">
+
+<head>
+  <title>SPAC-ECE</title>
+  <meta charset="UTF-8" />
+  <meta name="description" content="SPAC-ECE" />
+  <meta name="keywords" content="LERAMIZ, unica, creative, html" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <!-- Favicon -->
+  <link href="img/Favicon.ico" rel="shortcut icon" />
+
+  <!-- Google Fonts -->
+  <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro" rel="stylesheet" />
+
+  <!-- Stylesheets -->
+  <link rel="stylesheet" href="css/bootstrap.min.css" />
+  <link rel="stylesheet" href="css/font-awesome.min.css" />
+  <link rel="stylesheet" href="css/animate.css" />
+  <link rel="stylesheet" href="css/owl.carousel.css" />
+  <link rel="stylesheet" href="css/style.css" />
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" />
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script>
+
+  <link rel="stylesheet" type="text/css" href="Styles.css" />
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+  <link rel="stylesheet" type="text/css" href="css/jquery.convform.css" />
+  <link rel="stylesheet" type="text/css" href="css/responsive.css" />
+  <link rel="stylesheet" type="text/css" href="css/jquery-ui.css" />
+
+  <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+</head>
+
+<body>
+  <!-- Page Preloder -->
+  <div id="preloder">
+    <div class="loader"></div>
+  </div>
+
+  <!-- Header section -->
+  <header class="header-section" style="background-color: orange">
+    <div class="header-top" style="position: absolute; top: 15px; left: 80%">
+      <div class="container">
+        <div class="row">
+          <div class="col-lg-12 text-lg-right header-top-right">
+            <div class="top-social">
+              <a href="https://www.facebook.com/SpacECEIn"><i class="fa fa-facebook"></i></a>
+              <a href="https://www.instagram.com/spacece.in/"><i class="fa fa-instagram"></i></a>
+              <a href="https://t.me/joinchat/EtMJq_3BKL4zNGE1"><i class="fa fa-telegram" aria-hidden="true"></i></a>
+              <a href="https://www.linkedin.com/company/spacece-co/"><i class="fa fa-linkedin"></i></a>
+            </div>
+            <br /><br />
+
+            <div class="user-panel">
+              <a href="c_signup.php" style="color: black"><i class="fa fa-user-circle-o" style="color: black"></i>
+                <span>Sign Up</span>
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div>
+      <div class="row">
+        <div>
+          <div>
+            <div style="padding: -30px 20px">
+              <i class="fa fa-bars"></i>
+            </div>
+            <ul class="main-menu" style="margin-left: 60px">
+              <a href="http://educationfoundation.space/spacece/c_index.html" style="color: black">
+                <img src="img/space.jpg" alt="" style="width: 6%" />
+              </a>
+              <li>
+                <a href="http://educationfoundation.space/spacece/c_index.html" style="color: black"><i class="fa fa-home"></i>HOME</a>
+              </li>
+              <li>
+                <a href="http://educationfoundation.space/ConsultUs/about.html" style="color: black"><i class="fa fa-users"></i> ABOUT US</a>
+              </li>
+              <li>
+                <a href="http://educationfoundation.space/ConsultUs/contact.html" style="color: black"><i class="fa fa-envelope" style="color: black"></i>
+                  FEEDBACK</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+  </header>
+  <!-- Header section end -->
+
+  <!-- Hero section -->
+  <section class="hero-section set-bg" data-setbg="" style="width: 100%; height: 40%">
+    <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
+      <div class="carousel-inner">
+        <div class="carousel-item active">
+          <img class="d-block w-100" src="img/d7.jpg" alt="First slide" style="width: 100%; height: 30%" />
+        </div>
+        <div class="carousel-item">
+          <img class="d-block w-100" src="img/d8.jpg" alt="Second slide" />
+        </div>
+      </div>
+      <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+        <span class="sr-only">Previous</span>
+      </a>
+      <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+        <span class="sr-only">Next</span>
+      </a>
+    </div>
+  </section>
+  <br /><br /><br />
+
+  <!-- Hero section end -->
+  <!-- Services section -->
+  <!--<section class="services-section spad set-bg" data-setbg="img/org.png">
+		<div class="container">
+			<div class="row">
+				<div class="col-lg-6">				
+					<img src="img/service.jpg" alt="">
+				</div>
+				<div class="col-lg-5 offset-lg-1 pl-lg-0">
+					<div class="section-title text-white">
+						<h3>OUR SERVICES</h3>
+						<p>We provide the perfect service for </p>
+					</div>
+					<div class="services">
+						<div class="service-item">
+						<i class="fa fa-comments"></i>
+								<div class="service-text">
+								<h5>Consultant Service</h5>
+								<p>We provide you with the best services which is best for your family and which suits your pocket.</p>
+							</div>
+						</div>
+						<div class="service-item">
+							<i class="fa fa-home"></i>
+							<div class="service-text">
+								<h5>Properties Management</h5>
+								<p>We manage your property considering as our own and give you the best possible solution regarding it.</p>
+							</div>
+						</div>
+						<div class="service-item">
+							<i class="fa fa-briefcase"></i>
+							<div class="service-text">
+								<h5>Renting and Selling</h5>
+								<p>Enjoying various services provided by us without any mid-man, Book your dream home today!.</p>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>-->
+  <!-- Services section end -->
+  <!-- Blog section --><br /><br /><br /><br /><br /><br /><br />
+  <hr />
+  <br /><br /><br /><br /><br /><br />
+  <hr />
+  <section class="blog-section spad">
+    <div class="container">
+      <div class="section-title text-center">
+        <h3>OUR RECENT PROJECTS</h3>
+      </div>
+      <div class="row">
+        <div class="col-lg-4 col-md-6 blog-item">
+          <a href="http://localhost/spacece/ConsultUs/index.html?cat=consult">
+            <img src="img/icon_consultus.jpeg" alt="CONSULT US" width="300" height="300" /></a>
+          <h5><a href="#"> </a></h5>
+          <div class="blog-meta">
+            <!--<span><i class="fa fa-user"></i>Manas Sinkar</span>
+						<span><i class="fa fa-clock-o"></i>25 Jan 2019</span>-->
+          </div>
+          <p></p>
+        </div>
+        <div class="col-lg-4 col-md-6 blog-item">
+          <a href="/SpacTube/view.php?cat=spacetube">
+            <img src="img/icon_spacetube.jpeg" alt="SPACTUBE" width="300" height="300" /></a>
+          <h5><a href="#"> </a></h5>
+          <div class="blog-meta">
+            <!--<span><i class="fa fa-user"></i>Parth Thosani</span>
+						<span><i class="fa fa-clock-o"></i>04 Feb 2019</span>-->
+          </div>
+          <p></p>
+        </div>
+        <div class="col-lg-4 col-md-6 blog-item">
+          <a href="http://educationfoundation.space/moodle/login/index.php?cat=moodle">
+            <img src="img/icon_learnonapp.jpeg" alt="" width="300" height="300" /></a>
+          <h5><a href="#"> </a></h5>
+          <div class="blog-meta">
+            <!--<span><i class="fa fa-user"></i>Parth Thosani</span>
+						<span><i class="fa fa-clock-o"></i>04 Feb 2019</span>-->
+          </div>
+          <p></p>
+        </div>
+        <div class="col-lg-4 col-md-6 blog-item">
+          <a href="<?= dirname(__DIR__) ?>/Khanstore">
+            <img src="img/icon_libforsmalls.jpeg" alt="" width="300" height="300" /></a>
+          <h5><a href="#"> </a></h5>
+          <div class="blog-meta">
+            <!--<span><i class="fa fa-user"></i>Parth Thosani</span>
+						<span><i class="fa fa-clock-o"></i>04 Feb 2019</span>-->
+          </div>
+          <p></p>
+        </div>
+        <div class="col-lg-4 col-md-6 blog-item">
+          <a href="http://localhost/spacece/spacec_active">
+            <img src="img/icon_spaceactive.jpeg" alt="TOY LIBRARY" width="300" height="300" /></a>
+          <h5><a href="#"> </a></h5>
+          <div class="blog-meta">
+            <!--<span><i class="fa fa-user"></i>Parth Thosani</span>
+						<span><i class="fa fa-clock-o"></i>04 Feb 2019</span>-->
+          </div>
+          <p></p>
+        </div>
+      </div>
+    </div>
+    <!-- Blog section end -->
+
+    <!-- Clients section -->
+    <!--<div class="clients-section" >
+		<div class="container" >
+			<div class="clients-slider owl-carousel" style="background-color: or;">
+				<a href="#">
+					<img src="img/partner/1.png" alt="">
+				</a>
+				<a href="#">
+					<img src="img/partner/2.png" alt="">
+				</a>
+				<a href="#">
+					<img src="img/partner/3.png" alt="">
+				</a>
+				<a href="#">
+					<img src="img/partner/4.png" alt="">
+				</a>
+				<a href="#">
+					<img src="img/partner/5.png" alt="">
+				</a>
+			</div>
+		</div>
+	</div>-->
+    <!-- Clients section end -->
+
+    <!--Benefits-->
+    <div class="container">
+      <div class="section-title">
+        <h3 class="text-center">IF YOU REGISTER WITH US : BENIFITS</h3>
+        <p></p>
+      </div>
+      <div class="row">
+        <div class="col-lg-4 col-md-6 blog-item">
+          <p>
+            <i class="fa fa-check-circle" style="color: black"></i>
+            <span>
+              Wide varity of stuffs, which helps your children to grow
+              physically as well as mentally
+            </span>
+          </p>
+        </div>
+        <br /><br />
+
+        <div class="col-lg-4 col-md-6 blog-item">
+          <p>
+            <i class="fa fa-check-circle" style="color: black"></i>
+            <span>Access to various specialists</span>
+          </p>
+        </div>
+
+        <div class="col-lg-4 col-md-6 blog-item">
+          <p>
+            <i class="fa fa-check-circle" style="color: black"></i>
+            <span>Privacy & availability</span>
+          </p>
+        </div>
+
+        <div class="col-lg-4 col-md-6 blog-item">
+          <p>
+            <i class="fa fa-check-circle" style="color: black"></i>
+            <span>Comfort and Convenience</span>
+          </p>
+        </div>
+        <div class="col-lg-4 col-md-6 blog-item">
+          <p>
+            <i class="fa fa-check-circle" style="color: black"></i>
+            <span>Cost-effective and Time-saving</span>
+          </p>
+        </div>
+
+        <div class="col-lg-4 col-md-6 blog-item">
+          <p>
+            <i class="fa fa-check-circle" style="color: black"></i>
+            <span>Realiable and Trust-worthy</span>
+          </p>
+        </div>
+      </div>
+    </div>
+  </section>
+
+  <!---benefits section close-->
+
+  <!---offers section-->
+  <section class="hero-section set-bg" data-setbg="" style="width: 100%; height: 90%">
+    <div class="container">
+      <div class="section-title text-center">
+        <h3>GET THE BEST OFFERS HERE</h3>
+        <p></p>
+      </div>
+      <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
+        <div class="carousel-inner">
+          <div class="carousel-item active">
+            <img class="d-block w-100" src="img/o1.jpg" alt="First slide" style="width: 10%; height: 10%" />
+          </div>
+          <div class="carousel-item">
+            <img class="d-block w-100" src="img/o2.jpg" alt="Second slide" style="width: 100%; height: 30%" />
+          </div>
+        </div>
+        <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+          <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+          <span class="sr-only">Previous</span>
+        </a>
+        <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+          <span class="carousel-control-next-icon" aria-hidden="true"></span>
+          <span class="sr-only">Next</span>
+        </a>
+      </div>
+    </div>
+  </section>
+  <br /><br />
+
+  <!---offers close-->
+
+  <!-- ChatBot -->
+  <div class="chat_icon">
+    <i class="fa fa-comments" aria-hidden="true" style="color: black; border: 2px solid black"></i>
+  </div>
+
+  <div class="chat_box">
+    <div class="my-conv-form-wrapper">
+      <form action="" method="GET" class="hidden">
+        <select data-conv-question="Hello! How can I help you" name="category">
+          <option value="WebDevelopment">Toys Booking ?</option>
+          <option value="DigitalMarketing">Booking Consultants?</option>
+        </select>
+
+        <div data-conv-fork="category">
+          <div data-conv-case="WebDevelopment">
+            <input type="text" name="domainName" data-conv-question="Please, tell me your domain name" />
+          </div>
+          <div data-conv-case="DigitalMarketing" data-conv-fork="first-question2">
+            <input type="text" name="companyName" data-conv-question="Please, enter your company name" />
+          </div>
+        </div>
+
+        <input type="text" name="name" data-conv-question="Please, Enter your name" />
+
+        <input type="text" data-conv-question="Hi {name}, <br> It's a pleasure to meet you." data-no-answer="true" />
+
+        <input data-conv-question="Enter your e-mail" data-pattern="^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$" type="email" name="email" required placeholder="What's your e-mail?" />
+
+        <select data-conv-question="Please Conform">
+          <option value="Yes">Conform</option>
+        </select>
+      </form>
+    </div>
+  </div>
+
+  <!--session close-->
+
+  <!-- Footer section -->
+  <footer class="footer-section set-bg" style="
+        background-color: orange;
+        border-collapse: collapse;
+        border: 2px solid navy;
+        opacity: 0.7;
+        padding: 30px 30px;
+      ">
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-3 col-md-6 footer-widget">
+          <img src="img/logo1.png" alt="" />
+
+          <div class="social">
+            <a href="https://www.facebook.com/SpacECEIn"><i class="fa fa-facebook"></i></a>
+            <a href="https://www.instagram.com/spacece.in/"><i class="fa fa-instagram"></i></a>
+            <a href="https://t.me/joinchat/EtMJq_3BKL4zNGE1"><i class="fa fa-telegram" aria-hidden="true"></i></a>
+            <a href="https://www.linkedin.com/company/spacece-co/"><i class="fa fa-linkedin"></i></a>
+          </div>
+        </div>
+        <div class="col-lg-3 col-md-6 footer-widget">
+          <p style="color: black">
+            Still delaying your child's health concerns ?
+          </p>
+          <p style="color: black">Connect with India's top doctors online</p>
+        </div>
+        <div class="col-lg-3 col-md-6 footer-widget">
+          <div class="contact-widget">
+            <h5 class="fw-title text-center" style="color: black">
+              CONTACT US
+            </h5>
+            <p style="color: black">
+              <a href="http://www.spacece.in/" style="color: black">
+                <i class="fa fa-map-marker"></i>
+                SPACE-ECE
+              </a>
+            </p>
+            <p style="color: black">
+              <a href="tel: +919096305648" target="_blank" rel="noopener" style="color: black">
+                <i class="fa fa-phone" style="color: black"></i>
+                +91 90963 05648
+              </a>
+            </p>
+            <p style="color: black">
+              <a href="mailto:events@spacece.co" target="_blank" rel="noopener" style="color: black">
+                <i class="fa fa-envelope" style="color: black"></i>
+                contactus@spacece.co
+              </a>
+            </p>
+            <p style="color: black">
+              <i class="fa fa-clock-o" style="color: black"></i>
+              Mon - Sat, 8AM - 6PM
+            </p>
+          </div>
+        </div>
+
+        <div class="col-lg-3 col-md-6 footer-widget">
+          <div class="newslatter-widget">
+            <h5 class="fw-title" style="color: black; text-align: center">
+              NEWSLETTER
+            </h5>
+            <p style="color: black">
+              Subscribe your email to get the latest news and new offer also
+              discount
+            </p>
+            <form class="footer-newslatter-form">
+              <input type="text" placeholder="Email address" />
+              <button style="cursor: pointer" type="submit">
+                <i class="fa fa-send"></i>
+              </button>
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+  </footer>
+  <p class="font_10" style="line-height: 1.8em; text-align: center; font-size: 20px">
+    <span style="font-size: 20px"><span class="color_15">&copy;2021 by SpacECE INDIA FOUNDATION</span></span>
+  </p>
+  <!--====== Javascripts & Jquery ======-->
+  <script src="js/jquery-3.2.1.min.js"></script>
+  <script src="js/bootstrap.min.js"></script>
+  <script src="js/owl.carousel.min.js"></script>
+  <script src="js/masonry.pkgd.min.js"></script>
+  <script src="js/magnific-popup.min.js"></script>
+  <script src="js/main.js"></script>
+  <script type="js/jquery.js"></script>
+  <script type="text/javascript" src="js/jquery-3.1.1.min.js"></script>
+  <script type="text/javascript" src="js/jquery.convform.js"></script>
+  <script type="text/javascript" src="js/custom.js"></script>
+  <script type="text/javascript" src="js/jquery-1.12.4.js"></script>
+  <script type="text/javascript" src="js/jquery-ui.js"></script>
+  <script type="text/javascript" src="js/bootstrap.min.js"></script>
+</body>
+
+</html>
