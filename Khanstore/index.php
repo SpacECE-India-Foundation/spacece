f0b89c826332f882d289acda781d1ab6bdb901b4
vkm94 <varunmanila89@gmail.com>
2021-11-16 20:56:34 +0530

    index

--- Khanstore/index.php
+++ Khanstore/index.php
@@ -1,6 +1,7 @@
 <?php
 require "config/constants.php";
-session_start();
+//session_start();
+include_once 'includes/header1.php';
 
 if (isset($_SESSION["uid"])) {
 	header("location:profile.php");
@@ -12,9 +13,9 @@ if (isset($_SESSION["uid"])) {
 <head>
 	<meta charset="UTF-8">
 	<title>Khan Store</title>
-	<link rel="stylesheet" href="css/bootstrap.min.css" />
+	<!-- <link rel="stylesheet" href="css/bootstrap.min.css" /> -->
 	<script src="js/jquery2.js"></script>
-	<script src="js/bootstrap.min.js"></script>
+	<!-- <script src="js/bootstrap.min.js"></script> -->
 	<script src="main.js"></script>
 	<link rel="stylesheet" type="text/css" href="style.css">
 	<style></style>
@@ -24,7 +25,7 @@ if (isset($_SESSION["uid"])) {
 	<div class="wait overlay">
 		<div class="loader"></div>
 	</div>
-	<div class="navbar navbar-inverse navbar-fixed-top">
+	<!-- <div class="navbar navbar-inverse navbar-fixed-top">
 		<div class="container-fluid">
 			<div class="navbar-header">
 				<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#collapse" aria-expanded="false">
@@ -55,17 +56,17 @@ if (isset($_SESSION["uid"])) {
 										<div class="col-md-3">Sl.No</div>
 										<div class="col-md-3">Product Image</div>
 										<div class="col-md-3">Product Name</div>
-										<div class="col-md-3">Price in <?php echo CURRENCY; ?></div>
+										<div class="col-md-3">Price in <?php// echo CURRENCY; ?></div>
 									</div>
 								</div>
 								<div class="panel-body">
 									<div id="cart_product">
-										<!--<div class="row">
+										<div class="row">
 									<div class="col-md-3">Sl.No</div>
 									<div class="col-md-3">Product Image</div>
 									<div class="col-md-3">Product Name</div>
 									<div class="col-md-3">Price in $.</div>
-								</div>-->
+								</div>
 									</div>
 								</div>
 								<div class="panel-footer"></div>
@@ -99,7 +100,7 @@ if (isset($_SESSION["uid"])) {
 					</li>
 				</ul>
 			</div>
-		</div>
+		</div> -->
 	</div>
 	<p><br /></p>
 	<p><br /></p>
