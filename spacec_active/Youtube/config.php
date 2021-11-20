841cc2bf229c2f2739ce9164f73437f80d37de61
vkm94 <varunmanila89@gmail.com>
2021-11-20 15:41:09 +0530

    uploaded Youtube missing files

---
+++ spacec_active/Youtube/config.php
@@ -0,0 +1,22 @@
+<?php
+require_once 'vendor/autoload.php';
+require_once 'class-db.php';
+  
+define('GOOGLE_CLIENT_ID', '151284826547-71gu1qj8jco0l2gvqkt72bj8bj9k2u0a.apps.googleusercontent.com');
+define('GOOGLE_CLIENT_SECRET', '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');
+  
+$config = [
+    'callback' => 'http://localhost/spacece/spacec_ative/Youtube/callback.php',
+    'keys'     => [
+                    'id' => GOOGLE_CLIENT_ID,
+                    'secret' => GOOGLE_CLIENT_SECRET
+                ],
+    'scope'    => 'https://www.googleapis.com/auth/youtube https://www.googleapis.com/auth/youtube.upload',
+    'authorize_url_parameters' => [
+            'approval_prompt' => 'force', // to pass only when you need to acquire a new refresh token.
+            'access_type' => 'offline'
+    ]
+];
+  
+$adapter = new Hybridauth\Provider\Google( $config );
+
