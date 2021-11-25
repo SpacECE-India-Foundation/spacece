f9e2a09857a07055d09ce5fe3f40997292647536
Krishna <krishna.thorat20@vit.edu>
2021-11-25 18:12:55 +0530

    Khanstore minor update

--- Khanstore/cart.php
+++ Khanstore/cart.php
@@ -15,45 +15,7 @@ session_start();
 require "config/constants.php";
 
 ?>
-<!DOCTYPE html>
-<html>
 
-<!-- <head>
-	<meta charset="UTF-8">
-	<title>Khan Store</title>
-	<link rel="stylesheet" href="css/bootstrap.min.css" />
-	<script src="js/jquery2.js"></script>
-	<script src="js/bootstrap.min.js"></script>
-	<script src="main.js"></script>
-	<link rel="stylesheet" type="text/css" href="style.css" />
-</head>
-
-<body>
-	<div class="wait overlay">
-		<div class="loader"></div>
-	</div>
-	<div class="navbar navbar-inverse navbar-fixed-top">
-		<div class="container-fluid">
-			<div class="navbar-header">
-				<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#collapse" aria-expanded="false">
-					<span class="sr-only">navigation</span>
-					<span class="icon-bar"></span>
-					<span class="icon-bar"></span>
-					<span class="icon-bar"></span>
-				</button>
-				<a href="#" class="navbar-brand">Khan Store</a>
-			</div>
-			<div class="collapse navbar-collapse" id="collapse">
-				<ul class="nav navbar-nav">
-					<li><a href="index.php"><span class="glyphicon glyphicon-home"></span>Home</a></li>
-					<li><a href="index.php"><span class="glyphicon glyphicon-modal-window"></span>Product</a></li>
-				</ul>
-			</div>
-		</div>
-	</div> -->
-<p><br /></p>
-<p><br /></p>
-<p><br /></p>
 <div class="container">
 	<div class="row">
 
@@ -112,6 +74,3 @@ require "config/constants.php";
 	<?php
 	include_once '../common/footer_module.php';
 	?>
-	</body>
-
-</html>
