5e899f3cd35e8f3f24cb8664b266a7b2a209289a
vkm94 <varunmanila89@gmail.com>
2021-11-27 22:17:57 +0530

    updated

---
+++ spacece_auth/gmail/config.php
@@ -0,0 +1,14 @@
+<?php
+
+require_once 'vendor/autoload.php';
+
+$google_client=new Google_client();
+$google_client->setClientId('705362367676-tnd53a57sb9qve952d5po7u6sv79jsft.apps.googleusercontent.com');
+$google_client->setClientSecret('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');
+$google_client->setRedirectUri('http://localhost/asignment/form.php');
+
+//$google_client->setAccessType('offline');
+$google_client->addScope('email');
+$google_client->addScope('profile');
+
+ session_start();
--- spacece_auth/login.php
+++ spacece_auth/login.php
@@ -87,24 +87,24 @@ include_once '../common/header_module.php';
   <script>
     $('#google-button').on('click', function() {
       // Initialize with your OAuth.io app public key
-      OAuth.initialize('ikXj_8BxUB5IsVBnRuTxWCDqKc4');
-      // Use popup for OAuth
-      OAuth.popup('google').then(google => {
-        console.log('google:', google);
-        // Retrieves user data from oauth provider
-        // Prompts 'welcome' message with User's email on successful login
-        // #me() is a convenient method to retrieve user data without requiring you
-        // to know which OAuth provider url to call
-        google.me().then(data => {
-          console.log('me data:', data);
-          alert('Google says your email is:' + data.email + ".\nView browser 'Console Log' for more details");
-        });
-        // Retrieves user data from OAuth provider by using #get() and
-        // OAuth provider url
-        google.get('/plus/v1/people/me').then(data => {
-          console.log('self data:', data);
-        })
-      });
+      // OAuth.initialize('ikXj_8BxUB5IsVBnRuTxWCDqKc4');
+      // // Use popup for OAuth
+      // OAuth.popup('google').then(google => {
+      //   console.log('google:', google);
+      //   // Retrieves user data from oauth provider
+      //   // Prompts 'welcome' message with User's email on successful login
+      //   // #me() is a convenient method to retrieve user data without requiring you
+      //   // to know which OAuth provider url to call
+      //   google.me().then(data => {
+      //     console.log('me data:', data);
+      //     alert('Google says your email is:' + data.email + ".\nView browser 'Console Log' for more details");
+      //   });
+      //   // Retrieves user data from OAuth provider by using #get() and
+      //   // OAuth provider url
+      //   google.get('/plus/v1/people/me').then(data => {
+      //     console.log('self data:', data);
+      //   })
+      // });
     })
   </script>
 </body>
