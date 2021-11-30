e2255a7f6fe8d7c35c2769eaac96504d11d9ce30
vkm94 <varunmanila89@gmail.com>
2021-11-30 23:37:05 +0530

    gmail

--- spacece_auth/gmail/gmaillogin.php
+++ spacece_auth/gmail/gmaillogin.php
@@ -8,54 +8,54 @@ include '../db.php';
 //$login_button = '';
 
 //This $_GET["code"] variable value received after user has login into their Google Account redirct to PHP script then this variable value has been received
-// if(isset($_GET["code"]))
-// {
-//  //It will Attempt to exchange a code for an valid authentication token.
-//  $token = $google_client->fetchAccessTokenWithAuthCode($_GET["code"]);
+if(isset($_GET["code"]))
+{
+ //It will Attempt to exchange a code for an valid authentication token.
+ $token = $google_client->fetchAccessTokenWithAuthCode($_GET["code"]);
 
-//  //This condition will check there is any error occur during geting authentication token. If there is no any error occur then it will execute if block of code/
-//  if(!isset($token['error']))
-//  {
-//   //Set the access token used for requests
-//   $google_client->setAccessToken($token['access_token']);
+ //This condition will check there is any error occur during geting authentication token. If there is no any error occur then it will execute if block of code/
+ if(!isset($token['error']))
+ {
+  //Set the access token used for requests
+  $google_client->setAccessToken($token['access_token']);
 
-//   //Store "access_token" value in $_SESSION variable for future use.
-//   $_SESSION['access_token'] = $token['access_token'];
+  //Store "access_token" value in $_SESSION variable for future use.
+  $_SESSION['access_token'] = $token['access_token'];
 
-//   //Create Object of Google Service OAuth 2 class
-//   $google_service = new Google_Service_Oauth2($google_client);
+  //Create Object of Google Service OAuth 2 class
+  $google_service = new Google_Service_Oauth2($google_client);
 
-//   //Get user profile data from google
-//   $data = $google_service->userinfo->get();
+  //Get user profile data from google
+  $data = $google_service->userinfo->get();
 
-//   //Below you can find Get profile data and store into $_SESSION variable
-//  // $sql="Insert into social_login (email,name) VALUES('".$data['given_name']."','" .$data['email']."')";
-// //   if(!empty($data['given_name']))
-// //   {
-// //    $_SESSION['user_first_name'] = $data['given_name'];
-// //   }
+  //Below you can find Get profile data and store into $_SESSION variable
+ // $sql="Insert into social_login (email,name) VALUES('".$data['given_name']."','" .$data['email']."')";
+//   if(!empty($data['given_name']))
+//   {
+//    $_SESSION['user_first_name'] = $data['given_name'];
+//   }
 
-// //   if(!empty($data['family_name']))
-// //   {
-// //    $_SESSION['user_last_name'] = $data['family_name'];
-// //   }
+//   if(!empty($data['family_name']))
+//   {
+//    $_SESSION['user_last_name'] = $data['family_name'];
+//   }
 
-// //   if(!empty($data['email']))
-// //   {
-// //    $_SESSION['user_email_address'] = $data['email'];
-// //   }
+//   if(!empty($data['email']))
+//   {
+//    $_SESSION['user_email_address'] = $data['email'];
+//   }
 
-// //   if(!empty($data['gender']))
-// //   {
-// //    $_SESSION['user_gender'] = $data['gender'];
-// //   }
+//   if(!empty($data['gender']))
+//   {
+//    $_SESSION['user_gender'] = $data['gender'];
+//   }
 
-// //   if(!empty($data['picture']))
-// //   {
-// //    $_SESSION['user_image'] = $data['picture'];
-// //   }
-//  }
-// }
+//   if(!empty($data['picture']))
+//   {
+//    $_SESSION['user_image'] = $data['picture'];
+//   }
+ }
+}
 
 // //This is for check user has login into system by using Google account, if User not login into system then it will execute if block of code and make code for display Login link for Login using Google account.
 // if(!isset($_SESSION['access_token']))
