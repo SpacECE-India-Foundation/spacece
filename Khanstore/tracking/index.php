806b9fb07f3e5fb943f4d912d6db48ef3a49974a
vkm94 <varunmanila89@gmail.com>
2021-12-01 12:10:20 +0530

    live location

--- Khanstore/tracking/index.php
+++ Khanstore/tracking/index.php
@@ -12,8 +12,8 @@ $geoplugin = new geoPlugin();
 $geoplugin->locate();
  $geoplugin->longitude;
  $geoplugin->city;
- $geoplugin->latitude;
- $geoplugin->longitude;
+ $lat=$geoplugin->latitude;
+ $lan= $geoplugin->longitude;
  $geoplugin->regionName;
 // echo "Geolocation results for {$geoplugin->ip}: <br />\n".
 // 	"City: {$geoplugin->city} <br />\n".
@@ -62,11 +62,11 @@ $geoplugin->locate();
 <!DOCTYPE html>
 <html>
   <head>
-    <title>Simple Map</title>
+    <title>Tracking</title>
     <script src="https://polyfill.io/v3/polyfill.min.js?features=default"></script>
-   
+  
 	<style>
-
+    
 #map {
   height: 100%;
 }
@@ -78,6 +78,22 @@ body {
   margin: 0;
   padding: 0;
 }
+
+.custom-map-control-button {
+  background-color: #fff;
+  border: 0;
+  border-radius: 15px;
+  box-shadow: 0 1px 4px -1px rgba(0, 0, 0, 0.3);
+  margin: 10px;
+  padding: 0 0.5em;
+  font: 400 18px Roboto, Arial, sans-serif;
+  overflow: hidden;
+  height: 40px;
+  cursor: pointer;
+}
+.custom-map-control-button:hover {
+  background: #ebebeb;
+}
 </style>
    
   </head>
@@ -86,17 +102,59 @@ body {
 
     <!-- Async script executes immediately and must be after any DOM elements used in callback. -->
     <script
-      src="https://maps.googleapis.com/maps/api/js?key=YOUR_API_KEY&callback=initMap&v=weekly"
+      src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBSLlFldlf5BYfmidW8clTbDp4E6HMmG-U&callback=initMap&libraries=&v=weekly&channel=2"
       async
     ></script>
+   
 	<script>
-		let map;
+	let map, infoWindow;
 
 function initMap() {
   map = new google.maps.Map(document.getElementById("map"), {
-    center: { lat: -34.397, lng: 150.644 },
-    zoom: 8,
+    center: { lat:<?php echo $lat ?>, lng: <?php  echo $lan ?> },
+    zoom: 10,
   });
+  infoWindow = new google.maps.InfoWindow();
+
+  const locationButton = document.createElement("button");
+
+  locationButton.textContent = "Pan to Current Location";
+  locationButton.classList.add("custom-map-control-button");
+  map.controls[google.maps.ControlPosition.TOP_CENTER].push(locationButton);
+  locationButton.addEventListener("click", () => {
+    // Try HTML5 geolocation.
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(
+        (position) => {
+          const pos = {
+            lat: position.coords.latitude,
+            lng: position.coords.longitude,
+          };
+
+          infoWindow.setPosition(pos);
+          infoWindow.setContent("Location found.");
+          infoWindow.open(map);
+          map.setCenter(pos);
+        },
+        () => {
+          handleLocationError(true, infoWindow, map.getCenter());
+        }
+      );
+    } else {
+      // Browser doesn't support Geolocation
+      handleLocationError(false, infoWindow, map.getCenter());
+    }
+  });
+}
+
+function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+  infoWindow.setPosition(pos);
+  infoWindow.setContent(
+    browserHasGeolocation
+      ? "Error: The Geolocation service failed."
+      : "Error: Your browser doesn't support geolocation."
+  );
+  infoWindow.open(map);
 }
 		</script>
   </body>
