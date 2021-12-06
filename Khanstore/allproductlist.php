368195bcc8996edcb6d6b1e59ab135c4eaa13b43
snehalsalave <snehalsalave130@gmail.com>
2021-12-06 20:21:59 +0530

    allproductlist.php- add product brand.

--- Khanstore/allproductlist.php
+++ Khanstore/allproductlist.php
@@ -6,7 +6,7 @@ header('Access-Control-Allow-Origin: *');
 header("Access-Control-Allow-Methods: GET, OPTIONS");
 
 
-$servername = "localhost";
+$servername = "3.109.14.4";
 $username = "ostechnix";
 $password = "Password123#@!";
 $dbname = "khanstore";
