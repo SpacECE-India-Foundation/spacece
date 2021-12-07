5637ba4495cff8fee19ab95d40e9d5c33621d59f
Krishna Thorat <krishna.thorat20@vit.edu>
2021-12-07 15:30:27 +0530

    update

--- instamojo_api/payment.php
+++ instamojo_api/payment.php
@@ -56,7 +56,30 @@ if($payment_request_id) {
     
     $result = json_decode($response);
 
-    print_r($result);
+    $access_token = $result->access_token;
+
+      if($access_token) {
+        $ch = curl_init();
+
+        curl_setopt($ch, CURLOPT_URL, 'https://test.instamojo.com/v2/gateway/orders/payment-request/');
+        curl_setopt($ch, CURLOPT_HEADER, FALSE);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
+        curl_setopt($ch, CURLOPT_FOLLOWLOCATION, TRUE);
+        curl_setopt($ch, CURLOPT_HTTPHEADER,array('Authorization: Bearer '.$access_token));
+        
+        $payload = Array(
+            'id' => $payment_request_id
+        );
+        
+        curl_setopt($ch, CURLOPT_POST, true);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($payload));
+        $response = curl_exec($ch);
+        curl_close($ch); 
+
+        echo $response;
+
+        exit();
+      }
 
     exit();
 }
