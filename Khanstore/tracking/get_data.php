4af29a8d35eccf378e51d8b432cf8e288bc33d1a
vkm94 <varunmanila89@gmail.com>
2021-12-07 15:49:25 +0530

    update tracking from delivery boy

--- Khanstore/tracking/get_data.php
+++ Khanstore/tracking/get_data.php
@@ -1,5 +1,7 @@
 <?php
 include './db.php';
+if(isset($_GET['get'])){
+
  $user_id=$_POST['user'];
  $delivery_id=$_POST['track_id'];
     //$tracking_id=$_GET['tracking_id'];
@@ -9,5 +11,16 @@ $res = mysqli_query($con, $sql);
 $row=mysqli_fetch_assoc($res);
 
 echo json_encode($row);
+}
 //$lat2=$res['delivery_lat'];
 //$lang2=$res['delivery_lang'];
+if(isset($_GET['add'])){
+
+    $user_id=$_POST['user'];
+    $delivery_id=$_POST['track_id'];
+    $lat=$_POST['lat'];
+    $lang=$_POST['lang'];
+    $sql="UPDATE  tracking SET delivery_lat=$lat,delivery_lang=$lang Where user_id=$user_id and tracking_id=$delivery_id and is_Active='1' ";
+
+
+}
