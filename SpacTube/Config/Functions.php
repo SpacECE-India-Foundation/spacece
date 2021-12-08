c452582968ddb79726b7e29fb25561684ac0e7c3
vkm94 <varunmanila89@gmail.com>
2021-12-08 12:57:04 +0530

    updated db details

--- SpacTube/Config/Functions.php
+++ SpacTube/Config/Functions.php
@@ -3,10 +3,10 @@
 class Functions
 {
 
-    private $DBHOST = 'localhost';
-    private $DBUSER = 'root';
-    private $DBPASS = '';
-    private $DBNAME = 'gallery2';
+    private $DBHOST = '3.109.14.4';
+    private $DBUSER = 'ostechnix';
+    private $DBPASS = 'Password123#@!';
+    private $DBNAME = 'gallery';
     public $conn;
 
     public function __construct()
