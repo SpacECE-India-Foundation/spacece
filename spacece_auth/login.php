b8e407e6b1ccefc9634b60dc6ad53b2dd3ebc3e7
Krishna <krishna.thorat20@vit.edu>
2021-12-15 00:12:06 +0530

    registration major update

--- spacece_auth/login.php
+++ spacece_auth/login.php
@@ -1,28 +1,27 @@
 <?php
-session_start();
+include_once './header_local.php';
+include_once '../common/header_module.php';
+
 if (!isset($_SESSION['redirect_url']))
   $_SESSION['redirect_url'] = $_SERVER['HTTP_REFERER'];
 
 if (isset($_SESSION['current_user_id'])) {
   header("Location: index.php");
 }
-
-include_once './header_local.php';
-include_once '../common/header_module.php';
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
 
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/toastify-js/src/toastify.min.css"/>
-    <link rel="stylesheet" href="assets/css/style.css">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-    <title>Login</title>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/toastify-js/src/toastify.min.css" />
+  <link rel="stylesheet" href="assets/css/style.css">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+  <title>Login</title>
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -36,42 +35,53 @@ include_once '../common/header_module.php';
 
 </head>
 <style>
-.fa {
-  padding: 5px;
-  font-size: 30px;
-  width: 58px;
-  text-align: center;
-  text-decoration: none;
-  margin: 5px 2px;
-  border-radius: 50%;
-} 
+  .fa {
+    padding: 5px;
+    font-size: 30px;
+    width: 58px;
+    text-align: center;
+    text-decoration: none;
+    margin: 5px 2px;
+    border-radius: 50%;
+  }
 
-.fa-facebook {
-  background: #3B5998;
-  color: white;
-}
-.fa-google {
-  background: #dd4b39;
-  color: white;
-}
+  .fa-facebook {
+    background: #3B5998;
+    color: white;
+  }
+
+  .fa-google {
+    background: #dd4b39;
+    color: white;
+  }
 </style>
+
 <body>
   <div class="login-page">
     <h2>Login</h2>
     <form class="login-form" method="post" autocomplete="off">
-      <input type="email" placeholder="Enter Email" name="email" />
-      <input type="password" placeholder="Enter Password" name="password" />
-      <select name="type" id="user_type">
-        <option value="customer">Customer</option>
-        <option value="consultant">Consultant</option>
-      </select>
+      <div class="form-group">
+        <label for="name">Email</label>
+        <input type="email" class="form-control" placeholder="Enter Email" name="email" />
+      </div>
+      <div class="form-group">
+        <label for="name">Password</label>
+        <input type="password" class="form-control" placeholder="Enter Password" name="password" />
+      </div>
+      <div class="form-group">
+        <label for="name">User Type</label>
+        <select name="type" id="user_type">
+          <option value="customer">Customer</option>
+          <option value="consultant">Consultant</option>
+        </select>
+      </div>
       <button type="submit" name="login">Login</button>
       <br>
-             <!-- bug id -0000114 -->
-             <a id="google-button" href="https://www.google.com/account/about/" class="btn btn-block btn-social btn-google ">
+      <!-- bug id -0000114 -->
+      <a id="google-button" href="https://www.google.com/account/about/" class="btn btn-block btn-social btn-google ">
         <i class="fa fa-google"></i> Sign in with Google
       </a>
-      <a id="facebook-button"  href="https://www.facebook.com/" class="btn btn-block btn-social btn-facebook">
+      <a id="facebook-button" href="https://www.facebook.com/" class="btn btn-block btn-social btn-facebook">
         <i class="fa fa-facebook"></i> Sign in with Facebook
       </a>
 
@@ -79,25 +89,25 @@ include_once '../common/header_module.php';
 
       <?php
       require_once './gmail/gmaillogin.php';
-     
-    ?>
-      
-      <?php
-     include './facebook/index.php';
-    ?>
-     
 
-    <!-- <a id="google-button" class="btn btn-block btn-social btn-google">
+      ?>
+
+      <?php
+      include './facebook/index.php';
+      ?>
+
+
+      <!-- <a id="google-button" class="btn btn-block btn-social btn-google">
         <i class="fa fa-google"></i> Sign in with Google
       </a>  -->
-    <!-- <a id="facebook-button" class="btn btn-block btn-social btn-facebook">
+      <!-- <a id="facebook-button" class="btn btn-block btn-social btn-facebook">
         <i class="fa fa-facebook"></i> Sign in with Facebook
       </a>  -->
 
       <p class="message">Not registered? <a href="register.php">Create an account</a></p>
 
     </form>
-   
+
 
   </div>
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
@@ -106,8 +116,8 @@ include_once '../common/header_module.php';
   <script type="text/javascript" src="https://cdn.jsdelivr.net/npm/toastify-js"></script>
   <script type="text/javascript" src="main.js"></script>
 
-  
-   
+
+
 </body>
 
 </html>
