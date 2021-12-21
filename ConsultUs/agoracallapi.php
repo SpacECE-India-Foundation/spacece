c71573b7c4841064efd05ab61a991447832546af
vkm94 <varunmanila89@gmail.com>
2021-12-21 15:18:00 +0530

    added api

---
+++ ConsultUs/agoracallapi.php
@@ -0,0 +1,48 @@
+<?php
+include_once './indexDB.php';
+
+
+if(isset($_POST['create_call'])){
+
+    print_r($_POST);
+ //$user_id=$_SESSION['current_user_email'];
+
+    $consult_id=$_POST['consult_id'];
+ 
+ $user_id=$_POST['user_id'];
+
+ $channel_name=$_POST['channel_name'];
+			
+$sql="INSERT INTO agora_call(user_id,consult_id,channel_name) VALUES ('$user_id','$consult_id','$channel_name')";
+$result = mysqli_query($conn, $sql);
+if ($result) {
+    // header('location: login.php');
+    echo json_encode(array('status' => 'success'));
+    die();
+} else {
+    echo json_encode(array('status' => 'error', 'message' => "Error while Creating CAll!"));
+    die();
+}
+
+}
+if(isset($_POST['join_call'])){
+
+    //$user_id=$_SESSION['current_user_email'];
+    $user_id=$_POST['user_id'];
+   // $user_id=$_SESSION['current_user_email'];
+   // $consult_id=$_POST['consultant_id'];
+    //$channel_name=$_POST['channel_name'];
+                
+    $sql="SELECT * from agora_call where user_id='$user_id'";
+    $result = mysqli_query($conn, $sql);
+    if (mysqli_num_rows($result) > 0) {
+        // header('location: login.php');
+        echo json_encode(array('status' => 'success'));
+        //die();
+    } else {
+        echo json_encode(array('status' => 'error', 'message' => "No call Found!"));
+        die();
+    }
+    
+    }
+    
