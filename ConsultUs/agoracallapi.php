c96b60c281521a4daff9f1171f7f271f4519b305
vkm94 <varunmanila89@gmail.com>
2021-12-22 21:16:14 +0530

    updated

--- ConsultUs/agoracallapi.php
+++ ConsultUs/agoracallapi.php
@@ -3,38 +3,9 @@ include_once './indexDB.php';
 include("./php/src/RtcTokenBuilder.php");
 include("./php/src/RtmTokenBuilder.php");
 if(isset($_POST['create_call'])){
-    $consult_id=$_POST['consult_id'];
-    $user_id=$_POST['user_id'];
-    $channel_name=$_POST['channel_name'];
-    $appID = "464ff3e49fb3409494c0956edcec52e7";
-    $appCertificate = "21f542eedcde43a38f6c292abaa8c4c2";
-    $channelName =$user_id.$consult_id;
-    $uid = 0;
-    $uidStr = $user_id;
-    $role = RtcTokenBuilder::RoleAttendee;
-    $expireTimeInSeconds = 3600;
-    $currentTimestamp = (new DateTime("now", new DateTimeZone('UTC')))->getTimestamp();
-    $privilegeExpiredTs = $currentTimestamp + $expireTimeInSeconds;
-
-   
  
-   
-
-// $appID = "970CA35de60c44645bbae8a215061b33";
-// $appCertificate = "5CFd2fd1755d40ecb72977518be15d3b";
-
-// $role = RtmTokenBuilder::RoleRtmUser;
-// $expireTimeInSeconds = 3600;
-// $currentTimestamp = (new DateTime("now", new DateTimeZone('UTC')))->getTimestamp();
-// $privilegeExpiredTs = $currentTimestamp + $expireTimeInSeconds;
-
-//$token = RtmTokenBuilder::buildToken($appID, $appCertificate, $user, $role, $privilegeExpiredTs);
-//echo 'Rtm Token: ' . $token . PHP_EOL;
-
-
-$token = RtcTokenBuilder::buildTokenWithUid($appID, $appCertificate, $channelName, $uid, $role, $privilegeExpiredTs);
 //echo 'Token with int uid: ' . $token . PHP_EOL;
-    $channel_name="hello";
+    //$channel_name="hello";
 	//$token=$_POST['token'];	
     
     $sql="SELECT * from agora_call where user_id='$user_id' and consult_id='$consult_id' ORDER BY id DESC";
@@ -45,6 +16,25 @@ $token = RtcTokenBuilder::buildTokenWithUid($appID, $appCertificate, $channelNam
     if (mysqli_num_rows($result) > 0) {
         echo json_encode(array('status' => 'success','token'=>$token));
     }else{
+
+        $consult_id=$_POST['consult_id'];
+        $user_id=$_POST['user_id'];
+        $channel_name=$_POST['channel_name'];
+        $appID = "464ff3e49fb3409494c0956edcec52e7";
+        $appCertificate = "21f542eedcde43a38f6c292abaa8c4c2";
+        $channelName =$user_id.$consult_id;
+        $uid = 0;
+        $uidStr = $user_id;
+        $role = RtcTokenBuilder::RoleAttendee;
+        $expireTimeInSeconds = 3600;
+        $currentTimestamp = (new DateTime("now", new DateTimeZone('UTC')))->getTimestamp();
+        $privilegeExpiredTs = $currentTimestamp + $expireTimeInSeconds;
+    
+       
+     
+    
+    
+    $token = RtcTokenBuilder::buildTokenWithUid($appID, $appCertificate, $channelName, $uid, $role, $privilegeExpiredTs);
         $sql="INSERT INTO agora_call(user_id,consult_id,channel_name,token) VALUES ('$user_id','$consult_id','$channel_name','$token')";
         $result = mysqli_query($conn, $sql);
         
