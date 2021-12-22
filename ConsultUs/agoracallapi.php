4add779ce853ef9f3dabf30e94060a790f885c61
vkm94 <varunmanila89@gmail.com>
2021-12-22 20:52:25 +0530

    updated

--- ConsultUs/agoracallapi.php
+++ ConsultUs/agoracallapi.php
@@ -4,12 +4,12 @@ include("./php/src/RtcTokenBuilder.php");
 include("./php/src/RtmTokenBuilder.php");
 if(isset($_POST['create_call'])){
     $user_id=$_POST['user_id'];
-    //$channel_name=$_POST['channel_name'];
+    $channel_name=$_POST['channel_name'];
     $appID = "464ff3e49fb3409494c0956edcec52e7";
     $appCertificate = "21f542eedcde43a38f6c292abaa8c4c2";
-    $channelName = "test";
+    $channelName = $channel_name;
     $uid = 0;
-    $uidStr = "";
+    $uidStr = $user_id;
     $role = RtcTokenBuilder::RoleAttendee;
     $expireTimeInSeconds = 3600;
     $currentTimestamp = (new DateTime("now", new DateTimeZone('UTC')))->getTimestamp();
@@ -34,18 +34,30 @@ if(isset($_POST['create_call'])){
 $token = RtcTokenBuilder::buildTokenWithUid($appID, $appCertificate, $channelName, $uid, $role, $privilegeExpiredTs);
 //echo 'Token with int uid: ' . $token . PHP_EOL;
     $channel_name="hello";
-	//$token=$_POST['token'];		
-$sql="INSERT INTO agora_call(user_id,consult_id,channel_name,token) VALUES ('$user_id','$consult_id','$channel_name','$token')";
-$result = mysqli_query($conn, $sql);
+	//$token=$_POST['token'];	
+    
+    $sql="SELECT * from agora_call where user_id='$user_id' and consult_id='$consult_id' ORDER BY id DESC";
+    $result = mysqli_query($conn, $sql);
+    $row=mysqli_fetch_assoc($result);
+    $token=$row['token'];
+   //var_dump($row);
+    if (mysqli_num_rows($result) > 0) {
+        echo json_encode(array('status' => 'success','token'=>$token));
+    }else{
+        $sql="INSERT INTO agora_call(user_id,consult_id,channel_name,token) VALUES ('$user_id','$consult_id','$channel_name','$token')";
+        $result = mysqli_query($conn, $sql);
+        
+        if ($result) {
+            // header('location: login.php');
+            echo json_encode(array('status' => 'success','token'=>$token));
+            die();
+        } else {
+            echo json_encode(array('status' => 'error', 'message' => "Error while Creating CAll!"));
+            die();
+        }
+    }
 
-if ($result) {
-    // header('location: login.php');
-    echo json_encode(array('status' => 'success','token'=>$token));
-    die();
-} else {
-    echo json_encode(array('status' => 'error', 'message' => "Error while Creating CAll!"));
-    die();
-}
+
 
 }
 if(isset($_POST['join_call'])){
