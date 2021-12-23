05a04ec790d05ec33aa98046c3ea4534f1109a56
vkm94 <varunmanila89@gmail.com>
2021-12-23 11:52:41 +0530

    updated

--- ConsultUs/agoracallapi.php
+++ ConsultUs/agoracallapi.php
@@ -9,15 +9,31 @@ if(isset($_POST['consult_id'])){
 	//$token=$_POST['token'];	
     $consult_id=$_POST['consult_id'];
     $user_id=$_POST['user_id'];
-//     $sql="SELECT * from agora_call where user_id='$user_id' and consult_id='$consult_id' ORDER BY id DESC";
-//     $result = mysqli_query($conn, $sql);
-//     $row=mysqli_fetch_assoc($result);
-//     $token=$row['token'];
-//     $channelname=$row['channel_name'];
-//    //var_dump($row);
-//     if (mysqli_num_rows($result) > 0) {
-//         echo json_encode(array('status' => 'success','token'=>$token,'channelName'=>$channelname));
-//     }else{
+    $sql="SELECT * from agora_call where user_id='$user_id' and consult_id='$consult_id' ORDER BY id DESC";
+    $result = mysqli_query($conn, $sql);
+    $row=mysqli_fetch_assoc($result);
+    $token=$row['token'];
+    $channelname=$row['channel_name'];
+   //var_dump($row);
+    if (mysqli_num_rows($result) > 0) {
+        $consult_id=$_POST['consult_id'];
+        $user_id=$_POST['user_id'];
+        $channel_name=$user_id.$consult_id;
+        $appID = "464ff3e49fb3409494c0956edcec52e7";
+        $appCertificate = "21f542eedcde43a38f6c292abaa8c4c2";
+        $channelName =$user_id.$consult_id;
+        $uid = 0;
+        $uidStr = $user_id;
+        $role = RtcTokenBuilder::RoleAttendee;
+        $expireTimeInSeconds = 3600;
+        $currentTimestamp = (new DateTime("now", new DateTimeZone('UTC')))->getTimestamp();
+        $privilegeExpiredTs = $currentTimestamp + $expireTimeInSeconds;
+    
+    $token = RtcTokenBuilder::buildTokenWithUid($appID, $appCertificate, $channelName, $uid, $role, $privilegeExpiredTs);
+        $sql="UPDATE agora_call SET token ='$token' where user_id='$user_id' and consult_id='$consult_id' ";
+        $result = mysqli_query($conn, $sql);
+        echo json_encode(array('status' => 'success','token'=>$token,'channelName'=>$channelname));
+    }else{
 
         $consult_id=$_POST['consult_id'];
         $user_id=$_POST['user_id'];
@@ -48,7 +64,7 @@ if(isset($_POST['consult_id'])){
             echo json_encode(array('status' => 'error', 'message' => "Error while Creating CAll!"));
             die();
         }
-   // }
+}
 
 
 
