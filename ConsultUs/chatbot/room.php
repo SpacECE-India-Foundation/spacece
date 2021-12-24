4d0331cfa58f1e9ed108a0b45232f562e68cafd3
vkm94 <varunmanila89@gmail.com>
2021-12-24 14:37:19 +0530

    updated

--- ConsultUs/chatbot/room.php
+++ ConsultUs/chatbot/room.php
@@ -2,7 +2,7 @@
 $main_logo = "../img/logo/SpacECELogo.jpg";
 $module_logo = "../img/logo/ConsultUs.jpeg";
 $module_name = "ConsultUs";
-include_once '../common/header_module.php';
+include_once '../../common/header_module.php';
 ?>
 
 
@@ -126,6 +126,7 @@ background-image: linear-gradient(90deg,  white,orange,white);
 <!-- <input type="text" class="form-control" name="usermsg" id="usermsg" placeholder="add msg"><br> -->
 <button class="btn btn-secondary" name="submit" id="submit">send</button>
 
+</div>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-U1DAWAznBHeqEIlVSCgzq+c9gqGAJn5c/t99JyeKa9xxaYpSvHU5awsuZVVFIhvj" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.3/dist/umd/popper.min.js" integrity="sha384-eMNCOe7tC1doHpGoWe/6oMVemdAVTMs2xqW4mwXrXsW0L84Iytr2wi5v2QjrP/xp" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/js/bootstrap.min.js" integrity="sha384-cn7l7gDp0eyniUwwAZgrzD06kc/tftFf19TOAs2zVinnD/C7E91j9yyk5//jjpt/" crossorigin="anonymous"></script>
