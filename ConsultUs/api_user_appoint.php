23cf8bbbda6159958fa33969fcddb3f2c163a865
vkm94 <varunmanila89@gmail.com>
2021-12-25 17:11:52 +0530

    updated

--- ConsultUs/api_user_appoint.php
+++ ConsultUs/api_user_appoint.php
@@ -127,14 +127,15 @@ if($u_id || $c_id && $status=='Active' ){
   
     if($u_id){
         $sql="SELECT DISTINCT spaceece.users.u_name,spaceece.users.u_image,consultant_app.new_apointment.booking_id,
-        consultant_app.new_apointment.b_time , consultant_app.new_apointment.end_time FROM spaceece.users 
+        consultant_app.new_apointment.c_id,(SELECT spaceece.users.u_name from spaceece.users JOIN consultant_app.new_apointment 
+        WHERE spaceece.users.u_id=consultant_app.new_apointment.u_id and consultant_app.new_apointment.u_id='$u_id')AS c_name,(SELECT spaceece.users.u_image from spaceece.users JOIN consultant_app.new_apointment WHERE spaceece.users.u_id=consultant_app.new_apointment.u_id and consultant_app.new_apointment.u_id='$u_id')AS c_image,  consultant_app.new_apointment.end_time FROM spaceece.users 
         JOIN consultant_app.new_apointment
          WHERE spaceece.users.u_id = consultant_app.new_apointment.c_id AND consultant_app.new_apointment.u_id ='$u_id'";
     }if($c_id){
         $sql="SELECT DISTINCT spaceece.users.u_name,spaceece.users.u_image,consultant_app.new_apointment.booking_id,
-        consultant_app.new_apointment.b_time , consultant_app.new_apointment.end_time FROM spaceece.users 
-        JOIN consultant_app.new_apointment WHERE spaceece.users.u_id = consultant_app.new_apointment.u_id AND
-         consultant_app.new_apointment.c_id ='40'";
+        consultant_app.new_apointment.c_id,(SELECT spaceece.users.u_name from spaceece.users JOIN consultant_app.new_apointment WHERE spaceece.users.u_id=consultant_app.new_apointment.u_id and consultant_app.new_apointment.u_id='$u_id')AS c_name,(SELECT spaceece.users.u_image from spaceece.users JOIN consultant_app.new_apointment WHERE spaceece.users.u_id=consultant_app.new_apointment.u_id and consultant_app.new_apointment.u_id='$u_id')AS c_image,  consultant_app.new_apointment.end_time FROM spaceece.users 
+        JOIN consultant_app.new_apointment
+         WHERE spaceece.users.u_id = consultant_app.new_apointment.c_id AND consultant_app.new_apointment.u_id ='$c_id'";
     }
              $res = mysqli_query($conn,$sql);
              header('Content-Type:application/json');
