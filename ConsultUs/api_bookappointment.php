aeedf5c54c56d769bc5a4d9e07efb6aa695cd2a5
vkm94 <varunmanila89@gmail.com>
2021-12-25 19:03:26 +0530

    updated

--- ConsultUs/api_bookappointment.php
+++ ConsultUs/api_bookappointment.php
@@ -24,9 +24,25 @@ $res = mysqli_query($conn,$sql);
             $end=$row['end_time'];
           
             $date2=strtotime(date( $tme));
-            if($date1>$date2 ||$date1<$date2){
+            if($date1>$date2 || $date1<$date2){
                
-
+                if(strtotime($row['b_time'],strtotime("+{ $end} minutes")    )> $date1 || strtotime($row['b_time'],strtotime("+{ $end} minutes")    )< $date1 ){
+                    $sql = "INSERT INTO  new_apointment (u_id,c_id,b_time) VALUES('$u_id','$c_id','$b_time')";
+                    $res = mysqli_query($conn,$sql);
+                    header('Content-Type:application/json');
+            
+            
+                    //checking whether query is excuted or not
+                    if($res){
+                        echo json_encode(['status'=>'success','result'=>'found']);
+                        // count that data is there or not in database
+                        
+                       
+                    }
+                  
+                   }
+            }else{
+                echo json_encode(['status'=>'fail','msg'=>"UNABLE TO ADD DATA"]);
             }
 
           
@@ -34,20 +50,7 @@ $res = mysqli_query($conn,$sql);
         }
     }
 }
-        $sql = "INSERT INTO  new_apointment (u_id,c_id,b_time) VALUES('$u_id','$c_id','$b_time')";
-        $res = mysqli_query($conn,$sql);
-        header('Content-Type:application/json');
-
-
-        //checking whether query is excuted or not
-        if($res){
-            echo json_encode(['status'=>'success','result'=>'found']);
-            // count that data is there or not in database
-            
-           
-        } else{
-            echo json_encode(['status'=>'fail','msg'=>"UNABLE TO ADD DATA"]);
-        }
+        
 
     
                    
