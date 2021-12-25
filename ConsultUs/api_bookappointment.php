723e6ea05fe8b96faa3f1d8d17999e3309b28046
vkm94 <varunmanila89@gmail.com>
2021-12-25 20:04:19 +0530

    updated

--- ConsultUs/api_bookappointment.php
+++ ConsultUs/api_bookappointment.php
@@ -11,6 +11,7 @@ date($b_time);
 $date3=strtotime(date("Y-m-d h:i:sa"));
 date_default_timezone_set("Asia/Kolkata");
 $date1=strtotime(date($b_time));
+echo $date1;
 if($date3 < $date1){
     echo json_encode(['status'=>'fail','msg'=>"INVALID SELECTED DATE"]);
     
