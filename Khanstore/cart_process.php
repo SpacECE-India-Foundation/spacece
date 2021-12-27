8cf325621c23e357580b779acd83a52721822957
Krishna <krishna.thorat20@vit.edu>
2021-12-27 11:11:08 +0530

    change

--- Khanstore/cart_process.php
+++ Khanstore/cart_process.php
@@ -12,3 +12,8 @@ echo "<br><br>";
 echo '$_GET: <br>';
 print_r($_GET);
 echo "<br><br>";
+
+$new_array = array_merge($_SESSION, $_POST, $_GET);
+
+echo json_encode(['success' => false, 'message' => $new_array]);
+die();
