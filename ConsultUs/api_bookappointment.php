6bfd220f78874c9a43822ea67c8f7bdc964b3936
vkm94 <varunmanila89@gmail.com>
2021-12-27 10:59:40 +0530

    u

--- ConsultUs/api_bookappointment.php
+++ ConsultUs/api_bookappointment.php
@@ -11,6 +11,7 @@ $end_time=$_POST['end_time'];
 $date3=strtotime(date("Y-m-d h:i:sa"));
 date_default_timezone_set("Asia/Kolkata");
 $date1=strtotime(date($b_time));
+$date4=strtotime($b_time,strtotime("+{ $end_time} minutes"));
 
 if($date3 > $date1){
     echo json_encode(['status'=>'fail','date1'=>$date1,'date3'=>$date3,'msg'=>"INVALID SELECTED DATE"]);
@@ -37,7 +38,7 @@ $res = mysqli_query($conn,$sql1);
             if($date1>$date2 || $date1<$date2){
              
                
-                if(strtotime($row['b_time'],strtotime("+{ $end} minutes")   )> $date2 || strtotime($row['b_time'],strtotime("+{ $end} minutes")    )< $date2 ){
+                if(strtotime($row['b_time'],strtotime("+{ $end} minutes")   )> $date4 || strtotime($row['b_time'],strtotime("+{ $end} minutes")    )< $date4 ){
                     $sql = "INSERT INTO  new_apointment (u_id,c_id,b_time,end_time) VALUES('$u_id','$c_id','$b_time','$end_time')";
                     $res = mysqli_query($conn,$sql);
                     header('Content-Type:application/json');
