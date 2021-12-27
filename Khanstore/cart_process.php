836a3c9c456a7e46867592ac89661c10f43a3941
Krishna <krishna.thorat20@vit.edu>
2021-12-27 11:02:16 +0530

    minor update

---
+++ Khanstore/cart_process.php
@@ -0,0 +1,14 @@
+<?php
+session_start();
+
+echo '$_SESSION: <br>';
+print_r($_SESSION);
+echo "<br><br>";
+
+echo '$_POST: <br>';
+print_r($_POST);
+echo "<br><br>";
+
+echo '$_GET: <br>';
+print_r($_GET);
+echo "<br><br>";
