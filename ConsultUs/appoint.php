b7c45a32a9e4ca1a081d641364e1d23af9658c85
vkm94 <varunmanila89@gmail.com>
2021-12-28 11:11:46 +0530

    updated

--- ConsultUs/appoint.php
+++ ConsultUs/appoint.php
@@ -75,7 +75,10 @@ $stime = $_GET['stime'];
 
 <html>
 <head>
+
 <meta name="viewport" content="width=device-width, initial-scale=1">
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KyZXEAg3QhqLMpG8r+8fhAXLRk2vvoC2f3B09zVXn8CA5QIVfZOJ3BCsw2P0p/We" crossorigin="anonymous">
+
 <style>
 /* body {
   font-family: Arial, Helvetica, sans-serif;
@@ -202,8 +205,8 @@ a {
   
   ?>
 
-<form action="" method="POST">
-  <div class="container d-flex justify-content-center" style="width:80%">
+<form action="" method="POST" class=" d-flex justify-content-center">
+  <div class="container " style="width:80%">
     <h1>BOOK APPOINTMENT</h1>
     <p>
     </p>
@@ -255,6 +258,10 @@ include_once '../common/footer_module.php';
 }
 
 ?>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-U1DAWAznBHeqEIlVSCgzq+c9gqGAJn5c/t99JyeKa9xxaYpSvHU5awsuZVVFIhvj" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.3/dist/umd/popper.min.js" integrity="sha384-eMNCOe7tC1doHpGoWe/6oMVemdAVTMs2xqW4mwXrXsW0L84Iytr2wi5v2QjrP/xp" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/js/bootstrap.min.js" integrity="sha384-cn7l7gDp0eyniUwwAZgrzD06kc/tftFf19TOAs2zVinnD/C7E91j9yyk5//jjpt/" crossorigin="anonymous"></script>
+<script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
     </body>
 
 
