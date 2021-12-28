b008c1f51bdd89ff53ea151c9647d90884a3de90
vkm94 <varunmanila89@gmail.com>
2021-12-28 21:34:04 +0530

    updated

--- ConsultUs/chatbot/htcont.php
+++ ConsultUs/chatbot/htcont.php
@@ -25,8 +25,8 @@ if(mysqli_num_rows($result)>0)
     while($row= mysqli_fetch_assoc($result))
     {
            echo $res = "<div class='wrapper' id='msg'>";
-           if(isset($_POST['current_user_name'])){
-            echo  $res = $_POST['current_user_name'];
+           if(isset($_SESSION['current_user_name'])){
+            echo  $res = $_SESSION['current_user_name'];
            }else{
             echo  $res = $row['ip'];  
            }
