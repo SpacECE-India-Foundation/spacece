e4faf1a7d106e721542e1afe3efc257e3dff33a9
vkm94 <varunmanila89@gmail.com>
2021-12-28 15:15:05 +0530

    updated

--- ConsultUs/appoint.php
+++ ConsultUs/appoint.php
@@ -52,7 +52,7 @@ $c_id=$_GET['cid'];
 $b_id=$_GET['b_id'];
 $con_name=$_GET['con_name'];
  $cat_name=$_GET['cat_name'];
-  $sql="INSERT INTO `appointment`( `cid`, `category`,'username', `cname`,`bid`,`com_mob`) VALUES ('$c_id','$cat_name','$u_name','$con_name','$uid','$con_mob')";
+  $sql="INSERT INTO `appointment`( `cid`, `category`,`username`, `cname`,`bid`,`com_mob`) VALUES ('$c_id','$cat_name','$u_name','$con_name','$u_id','$u_mob')";
   $res= mysqli_query($conn,$sql);
   echo $sql;
   if(!$res){echo "<h3 style = 'color:white;'><center>sorry,unable to connect</center></h3>";}
