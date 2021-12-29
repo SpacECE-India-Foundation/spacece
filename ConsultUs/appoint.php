238dd3fb23c544ed4714503e8fa0be8d4e58d68b
vkm94 <varunmanila89@gmail.com>
2021-12-29 14:56:46 +0530

    updated

--- ConsultUs/appoint.php
+++ ConsultUs/appoint.php
@@ -186,51 +186,51 @@ a {
       if(isset($_POST["submit"]))
       { //1.getting data into variable
         
-      //     $full_name = $user_name;
-      //     $email = $user_email;
-      //     $mob = $user_mob;
-      //     // $bookid = $uid;
-      //      $atime = $_POST["atime"];
-      //      $adate = $_POST["adate"];
-      //    $status ="inactive";
-      //    // encrypt pass 
-      //    $time= date("H:i", strtotime($atime));
-      //    $time1=strtotime($time);
-      //    if(strtotime($c_from_time) > $time1 || strtotime($c_to_time) < $time1){
-      //      echo '<script>alert("Consultant Un available in selected Time");<script>';
+          $full_name = $user_name;
+          $email = $user_email;
+          $mob = $user_mob;
+          // $bookid = $uid;
+           $atime = $_POST["atime"];
+           $adate = $_POST["adate"];
+         $status ="inactive";
+         // encrypt pass 
+         $time= date("H:i", strtotime($atime));
+         $time1=strtotime($time);
+         if(strtotime($c_from_time) > $time1 || strtotime($c_to_time) < $time1){
+           echo '<script>alert("Consultant Un available in selected Time");<script>';
           
-      //      $_SESSION['add']= "Consultant Un available";
-      //    }else{
+           $_SESSION['add']= "Consultant Un available";
+         }else{
 
          
-      //    //2.inserting into database
-      //   $sql= " UPDATE `appointment` SET  `status`='$status',`time_appointment`='$atime',`date_appointment`='$adate' WHERE `bid`='$b_id'";
+         //2.inserting into database
+        $sql= " UPDATE `appointment` SET  `status`='$status',`time_appointment`='$atime',`date_appointment`='$adate' WHERE `bid`='$b_id'";
        
-      //   $res= mysqli_query($conn,$sql);
-      //    echo "<h3 style = 'color:white;'>$full_name<h3>";
-      //    echo "<h3 style = 'color:white;'>$email<h3>";
-      //    echo "<h3 style = 'color:white;'>$mob<h3>";
-      //    echo "<h3 style = 'color:white;'>$userid<h3>";
+        $res= mysqli_query($conn,$sql);
+         echo "<h3 style = 'color:white;'>$full_name<h3>";
+         echo "<h3 style = 'color:white;'>$email<h3>";
+         echo "<h3 style = 'color:white;'>$mob<h3>";
+         echo "<h3 style = 'color:white;'>$userid<h3>";
        
-      //    //3. checking data is inserted or not
-      //    if($res){
-      //        $_SESSION['add']= "<div style='color:green;'> appointment booked successfully</div>";         //creating session variable
-      //        // redirecting page
-      //        header("location:./alldoc.php");
-      //        //echo "<h3 style = 'color:white;'>database updated<h3>";
-      //    }
-      // else{
-      //     $_SESSION['add']= "failed to book appointment successfully";         //creating session variable
-      //     // redirecting page
-      //     header("location:./alldoc.php");
-      // //echo "<h3 style = 'color:white;'>database not updated<h3>";
-      // }
+         //3. checking data is inserted or not
+         if($res){
+             $_SESSION['add']= "<div style='color:green;'> appointment booked successfully</div>";         //creating session variable
+             // redirecting page
+             header("location:./alldoc.php");
+             //echo "<h3 style = 'color:white;'>database updated<h3>";
+         }
+      else{
+          $_SESSION['add']= "failed to book appointment successfully";         //creating session variable
+          // redirecting page
+          header("location:./alldoc.php");
+      //echo "<h3 style = 'color:white;'>database not updated<h3>";
+      }
       
-      //     echo '<div class="alert alert-warning alert-dismissible fade show" role="alert" style:"backgeound-color:orange">
-      //     <strong style="color:white;">sucess!</strong> <h3 style="color:white;">Your form is submitted sucessfully.</h3>
-      //     <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
-      //   </div>';
-      // }
+          echo '<div class="alert alert-warning alert-dismissible fade show" role="alert" style:"backgeound-color:orange">
+          <strong style="color:white;">sucess!</strong> <h3 style="color:white;">Your form is submitted sucessfully.</h3>
+          <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+        </div>';
+      }
   
      }
   
