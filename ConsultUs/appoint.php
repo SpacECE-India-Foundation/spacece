cc1c1fddcbf63424dda906a627968e7498edce2c
snehalsalave <snehalsalave130@gmail.com>
2021-12-29 12:44:34 +0530

    bug id-0000165,0000166.

--- ConsultUs/appoint.php
+++ ConsultUs/appoint.php
@@ -226,14 +226,14 @@ a {
   <input type="time" id="atime" name="atime" min="16:00" max="22:00" >
 <br><br>
     <label for="fullname"><b>Fullname</b></label>
-    <input type="text" value="<?php echo $u_name ?>" name="fullname" id="fullname" required readonly>
+    <input type="text" value="<?php echo $u_name ?>" onkeypress="return (event.charCode > 64 && event.charCode < 91) || (event.charCode > 96 && event.charCode < 123) || (event.charCode==32)"  name="fullname" id="fullname" required readonly>
 <label for="cname"><b>Consultant Name</b></label>
-    <input type="text" value="<?php echo $con_name ?>" name="cname" id="cname" required>
+    <input type="text" value="<?php echo $con_name ?>" onkeypress="return (event.charCode > 64 && event.charCode < 91) || (event.charCode > 96 && event.charCode < 123) || (event.charCode==32)" name="cname" id="cname" required>
 
     <label for="email"><b>Email</b></label>
     <input type="text" value="<?php echo $u_email ?>" name="email" id="email" required>
     <label for="mobile"><b>Mobile Number:</b></label>
-    <input type="text" value="<?php echo $u_mob ?>" name="mobile" id="mobile" required><br>
+    <input type="text" value="<?php echo $u_mob ?>"   maxlength="10" name="mobile" id="mobile" required><br>
     
     <hr>
   <input type="submit" name="submit" class="registerbtn" value="submit">
