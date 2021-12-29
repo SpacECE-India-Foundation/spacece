f73e883bbc874740870dd87f4407359f10d801f7
Krishna <krishna.thorat20@vit.edu>
2021-12-29 14:07:30 +0530

    update

--- spacece_auth/logout.php
+++ spacece_auth/logout.php
@@ -15,5 +15,6 @@ if (isset($_SESSION['current_user_type']))
 // $google_client->revokeToken();
 $redirect_url = $_SERVER['HTTP_REFERER'];
 session_destroy();
-header('Location:./index.php');
-//header("Location: " . $redirect_url);
+// header('Location:./index.php');
+header("Location: " . $redirect_url);
+exit();
