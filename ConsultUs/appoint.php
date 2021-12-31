1de91fc10e68bae83826b44f52d38d52943ef3f1
vkm94 <varunmanila89@gmail.com>
2021-12-31 22:56:37 +0530

    updated

--- ConsultUs/appoint.php
+++ ConsultUs/appoint.php
@@ -82,7 +82,7 @@ $b_id=$_GET['b_id'];
 $con_name=$_GET['con_name'];
  $cat_name=$_GET['cat_name'];
  
-  $sql="INSERT INTO `appointment`( `cid`, `category`,`username`,`email`cname`,`bid`,`mob`) VALUES ('$c_id','$cat_name','$u_name','$u_email','$con_name','$b_id','$u_mob')";
+  $sql="INSERT INTO `appointment`( `cid`, `category`,`username`,`cname`,`bid`,`mob`) VALUES ('$c_id','$cat_name','$u_name','$con_name','$b_id','$u_mob')";
 
   
   $res= mysqli_query($conn,$sql);
