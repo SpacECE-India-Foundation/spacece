4644bdb0766ef042847f719a50c537dc0e11528a
vkm94 <varunmanila89@gmail.com>
2021-12-31 15:50:11 +0530

    updated

--- ConsultUs/appoint.php
+++ ConsultUs/appoint.php
@@ -345,9 +345,9 @@ $.ajax({
   },url:'./c_booking_ajax.php',
   success:function(data){
 
-    alert(data)
     
-    alert(answer)
+    
+  
     swal("Good job!", "Booking Id"+data.b_id+"", "success") 
 //window.location.href="./cdetails.php?category=all"
 //swal("Good job!", data, "success");
