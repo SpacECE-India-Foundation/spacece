596b6f47005f421ede9eed9a53f85a6b20b2d532
vkm94 <varunmanila89@gmail.com>
2022-01-01 14:03:37 +0530

    updated

--- spacec_active/db.php
+++ spacec_active/db.php
@@ -2,7 +2,7 @@
 
 
 $mysqli1 = new mysqli('3.109.14.4', 'ostechnix', 'Password123#@!', 'space_active');
-
+// $mysqli1 = new mysqli('localhost', 'root', '', 'space_active');
 // Check connection
 if ($mysqli1->connect_errno) {
     echo "Failed to connect to MySQL: " . $mysqli->connect_error;
