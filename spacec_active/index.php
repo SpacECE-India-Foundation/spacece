dcb4182b284e3a76aad1e840fc03671c5ce09c03
vkm94 <varunmanila89@gmail.com>
2022-01-01 17:33:43 +0530

    updated

--- spacec_active/index.php
+++ spacec_active/index.php
@@ -348,6 +348,23 @@ include_once '../common/banner.php';
                         echo "</div>";
                     }
                     echo "</div>";
+                }else{
+                    include_once 'Youtube/class-db.php';
+                   
+                    echo "<div class='row'>";
+                    $db = new DB();
+                    $videos = $db->get_all_Videos();
+                    foreach ($videos as $video) {
+                        $video_id = isset($video['video_id']) ? ($video['video_id']) : NULL;
+                       
+                        echo "<div class='col-md-6'>";
+                        echo '<iframe width="250" height="180"
+                               src="https://www.youtube.com/embed/' .  $video_id . '"
+                               frameBorder="0" allow="accelerometer";encrypted-media;gyroscope;picture-in-picture"allowfullscreen>
+                               </iframe>';
+                        echo "</div>";
+                    }
+                    echo "</div>";
                 }
                 ?>
                 <div class="modal-footer">
