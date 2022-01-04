da56128346f5515f41f5556a743a5b05064e72d3
vkm94 <varunmanila89@gmail.com>
2022-01-04 16:34:11 +0530

    updated

--- ConsultUs/api_bookappointment.php
+++ ConsultUs/api_bookappointment.php
@@ -25,7 +25,7 @@ $count= mysqli_num_rows($res1);
 $sno =1;
 if($count>0){
 while($row = mysqli_fetch_assoc($res1)){
- if( (strtotime(date($row['c_to_time']))) < $time || (strtotime(date($row['c_from_time']))) > $time){
+ if( (strtotime(date($row['c_to_time']))) > $time || (strtotime(date($row['c_from_time']))) < $time){
     echo json_encode(['status'=>'fail','time'=>$time,'from'=>strtotime(date($row['c_from_time'])),'to'=> strtotime(date($row['c_to_time'])),'b_time'=>$b_time,'date1'=>$date1,'date3'=>$date3,'date4'=>$date4,'msg'=>"CONSULTANT NOT AVAILABLE"]);
  }
 else{
