bb1e4f9f95aaa29ea65ab2cc303a7a5bb1c2a462
vkm94 <varunmanila89@gmail.com>
2022-01-04 11:17:47 +0530

    updated

--- ConsultUs/api_bookappointment.php
+++ ConsultUs/api_bookappointment.php
@@ -1,7 +1,7 @@
 <?php
  include("indexDB.php");
 
-
+ date_default_timezone_set("Asia/Kolkata");
 $u_id=$_POST['u_id'];
 $c_id=$_POST['c_id'];
 
@@ -10,8 +10,9 @@ $end_time=$_POST['end_time'];
  $b_time= date($b_time);
 
 $date3=strtotime(date("Y-m-d h:i:sa"));
-date_default_timezone_set("Asia/Kolkata");
+
 $date1=strtotime($b_time);
+
 //$date1= strtotime(date('2022-04-21 16:55:01'));;
 $date4=strtotime($b_time,strtotime("+{ $end_time} minutes"));
 $sql2="SELECT spaceece.consultant.c_from_time,spaceece.consultant.c_to_time from spaceece.consultant join spaceece.users where  spaceece.users.u_id=spaceece.consultant.u_id AND spaceece.users.u_id='$c_id' ";
