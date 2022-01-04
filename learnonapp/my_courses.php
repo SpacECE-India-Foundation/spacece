36a80b61006346685322ba260384fa39fcb96490
Krishna <krishna.thorat20@vit.edu>
2022-01-04 21:30:27 +0530

    update

---
+++ learnonapp/my_courses.php
@@ -0,0 +1,20 @@
+<?php
+include_once './header_local.php';
+include_once '../common/header_module.php';
+//include_once 'includes/header1.php'
+include_once '../common/banner.php';
+?>
+<!-- <head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
+</head> -->
+<div class="my_course_container">
+    <div id="my_courses">
+
+    </div>
+</div>
+<?php
+include_once '../common/footer_module.php';
+?>
