6af9fe6b512d808222683bd8483120c8b275eafb
vkm94 <varunmanila89@gmail.com>
2022-01-05 15:38:55 +0530

    updated

--- ConsultUs/api_bookappointment.php
+++ ConsultUs/api_bookappointment.php
@@ -38,7 +38,7 @@ while($row = mysqli_fetch_assoc($res1)){
     }else{
 
    // $b_date=date($_POST['b_date']);
-    $sql1="SELECT * from new_apointment where c_id='$c_id' and b_date='date($b_date)'";
+    $sql1="SELECT * from new_apointment where c_id='$c_id' and b_date='$b_date'";
     $res2 = mysqli_query($conn,$sql1);
 
             // count that data is there or not in database
