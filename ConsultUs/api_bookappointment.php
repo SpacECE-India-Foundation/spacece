f91eb1983b5322e84005f40460e0a720fe7a82c3
vkm94 <varunmanila89@gmail.com>
2022-01-05 11:24:51 +0530

    updated

--- ConsultUs/api_bookappointment.php
+++ ConsultUs/api_bookappointment.php
@@ -52,10 +52,10 @@ while($row = mysqli_fetch_assoc($res1)){
             
                 $date2=strtotime($row['booking_time'],strtotime("+{ $end} minutes") );
           
-                if($date5 > $time || $date5 < $date2){
+                if($date5 > $time || $date5 > $date2){
         
                 
-                     if($date5 < $time  || $date5 > $date2){
+                     if($date5 < $time  || $date5 < $date2){
                     //     if ($date1 > $date2 ||  $date1 < $date2 ){
                         
                             $sql = "INSERT INTO  new_apointment (u_id,c_id,b_time,end_time,b_date,booking_time) VALUES('$u_id','$c_id','$b_time','$end_time','$b_date','$booking_time')";
