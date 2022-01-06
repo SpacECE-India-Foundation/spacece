6d6ed1af21b213213e82ea24ffd8672ecc7514fb
snehalsalave <snehalsalave130@gmail.com>
2022-01-06 15:03:16 +0530

    Recommdation api-recommendationsapi.php

---
+++ Khanstore/recommendationsapi.php
@@ -0,0 +1,57 @@
+<?php
+session_start();
+
+header('Access-Control-Allow-Origin: *');
+header("Access-Control-Allow-Methods: GET, OPTIONS");
+
+
+$servername = "3.109.14.4";
+$username = "ostechnix";
+$password = "Password123#@!";
+$dbname = "khanstore";
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+?>
+<?php
+	$userId = isset($_GET["userId"])? $_GET['userId'] : Null;
+
+error_reporting();
+
+if (isset($userId)) {
+    $sql = "SELECT * FROM `recommendationsapi` WHERE userId=" . $userId;
+    $res = mysqli_query($conn,$sql);
+    header('Content-Type:application/json');
+}
+else {
+    $sql = "SELECT * FROM `recommendationsapi` LIMIT 35";
+    $res = mysqli_query($conn, $sql);
+    header('Content-Type:application/json');
+}
+
+//checking whether query is excuted or not
+if ($res) {
+    // count that data is there or not in database
+    $count = mysqli_num_rows($res);
+    $sno = 1;
+    if ($count > 0) {
+        // we have data in database
+        while ($row = mysqli_fetch_assoc($res)) {
+
+            $arr[] = $row;   // making array of data
+
+        }
+        echo json_encode(['status' => 'success', 'data' => $arr, 'result' => 'found']);
+        //echo json_encode(['status'=>'success','result'=>'found']);
+
+
+    } else {
+        echo json_encode(['status' => 'failure', 'result' => 'not found']);
+    }
+}
+
+?>
