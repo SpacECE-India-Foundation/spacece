014e9cbac196d2aa0e454662eb4472ab2fc236b7
vkm94 <varunmanila89@gmail.com>
2022-01-07 10:48:47 +0530

    updated

--- spacec_active/ajax_add_activity.php
+++ spacec_active/ajax_add_activity.php
@@ -1,51 +1,51 @@
 <?php
-
- require_once './Youtube/config.php';
 var_dump($_POST);
- $act_name=$_POST['act_name'];
- $act_lvl=$_POST[' act_lvl'];
+//  require_once './Youtube/config.php';
 
- $act_dom=$_POST['act_dom'];
- $act_obj=$_POST['pl_desc'];
- $pl_name=$_POST['pl_desc'];
- $act_pro=$_POST['pl_desc'];
- $act_key=$_POST['pl_desc'];
- $act_mat=$_POST['pl_desc'];
- $act_asses=$_POST['pl_desc'];
- $act_ins=$_POST['pl_desc'];
- $act_date=$_POST['pl_desc'];
- $pl_desc=$_POST['pl_desc'];
+//  $act_name=$_POST['act_name'];
+//  $act_lvl=$_POST[' act_lvl'];
 
- $client = new Google_Client();
+//  $act_dom=$_POST['act_dom'];
+//  $act_obj=$_POST['pl_desc'];
+//  $pl_name=$_POST['pl_desc'];
+//  $act_pro=$_POST['pl_desc'];
+//  $act_key=$_POST['pl_desc'];
+//  $act_mat=$_POST['pl_desc'];
+//  $act_asses=$_POST['pl_desc'];
+//  $act_ins=$_POST['pl_desc'];
+//  $act_date=$_POST['pl_desc'];
+//  $pl_desc=$_POST['pl_desc'];
+
+//  $client = new Google_Client();
   
-// $db = new DB();
+// // $db = new DB();
   
-    $arr_token = (array) $db->get_access_token();
-    $accessToken = array(
-        'access_token' => $arr_token['access_token'],
-        'expires_in' => $arr_token['expires_in'],
-    );
+//     $arr_token = (array) $db->get_access_token();
+//     $accessToken = array(
+//         'access_token' => $arr_token['access_token'],
+//         'expires_in' => $arr_token['expires_in'],
+//     );
     
-    $client->setAccessToken($accessToken);  
-    $service = new Google_Service_YouTube($client);
+//     $client->setAccessToken($accessToken);  
+//     $service = new Google_Service_YouTube($client);
 
-// Define the $playlist object, which will be uploaded as the request body.
-$playlist = new Google_Service_YouTube_Playlist();
+// // Define the $playlist object, which will be uploaded as the request body.
+// $playlist = new Google_Service_YouTube_Playlist();
 
-// Add 'snippet' object to the $playlist object.
-$playlistSnippet = new Google_Service_YouTube_PlaylistSnippet();
-$playlistSnippet->setChannelId('UCSFXd8_Kp1a5ZHAaOejPiH');
-$playlistSnippet->setDescription($pl_desc);
-$playlistSnippet->setTitle($pl_name);
-$playlist->setSnippet($playlistSnippet);
+// // Add 'snippet' object to the $playlist object.
+// $playlistSnippet = new Google_Service_YouTube_PlaylistSnippet();
+// $playlistSnippet->setChannelId('UCSFXd8_Kp1a5ZHAaOejPiH');
+// $playlistSnippet->setDescription($pl_desc);
+// $playlistSnippet->setTitle($pl_name);
+// $playlist->setSnippet($playlistSnippet);
 
-// Add 'status' object to the $playlist object.
-$playlistStatus = new Google_Service_YouTube_PlaylistStatus();
-$playlistStatus->setPrivacyStatus('public');
-$playlist->setStatus($playlistStatus);
+// // Add 'status' object to the $playlist object.
+// $playlistStatus = new Google_Service_YouTube_PlaylistStatus();
+// $playlistStatus->setPrivacyStatus('public');
+// $playlist->setStatus($playlistStatus);
 
-$response = $service->playlists->insert('snippet,status', $playlist);
-print_r($response);
+// $response = $service->playlists->insert('snippet,status', $playlist);
+// print_r($response);
 
 
 
