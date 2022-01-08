fd1c11e141e0dc27d2795adc3e39c24b2d4517c9
vkm94 <varunmanila89@gmail.com>
2022-01-08 17:44:44 +0530

    updated

--- spacec_active/index.php
+++ spacec_active/index.php
@@ -387,7 +387,7 @@ include_once '../common/banner.php';
 
 </html>
 <?php
-include_once '../common/footer_module.php';
+// include_once '../common/footer_module.php';
 ?>
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
