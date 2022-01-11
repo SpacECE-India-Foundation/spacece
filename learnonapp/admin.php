b58c9d2a902234f709441d97c21b85be84a00336
Krishna <krishna.thorat20@vit.edu>
2022-01-11 10:16:10 +0530

    leanonapp admin

---
+++ learnonapp/admin.php
@@ -0,0 +1,11 @@
+<?php
+include_once './header_local.php';
+include_once '../common/header_module.php';
+include_once '../common/banner.php';
+
+print_r($_SESSION);
+?>
+
+<?php
+include_once '../common/footer_module.php';
+?>
