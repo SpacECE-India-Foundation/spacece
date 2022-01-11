22d21670d111653173c525c832057bc2aafb772c
vkm94 <varunmanila89@gmail.com>
2022-01-11 23:35:54 +0530

    updated

---
+++ spacec_active/admin.php
@@ -0,0 +1,16 @@
+<?php
+include_once './header_local.php';
+include_once '../common/header_module.php';
+include_once '../common/banner.php';
+
+// if (!isset($_SESSION['current_user_id']) && !isset($_SESSION['current_user_type'])) {
+//     header("Location: ./index.php");
+// }
+?>
+<div class="main-content text-centre" style="background: linear-gradient(to bottom right, #ffcc99 10%, #ffffff 100%);">
+<div id="admin-page">
+</div>
+</div>
+<?php
+include_once '../common/footer_module.php';
+?>
