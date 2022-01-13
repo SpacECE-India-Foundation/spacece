c99363dbf6be98d64c7095fdd354d3707b52d20e
vkm94 <varunmanila89@gmail.com>
2022-01-13 16:28:51 +0530

    updated

--- spacec_active/Youtube/index.php
+++ spacec_active/Youtube/index.php
@@ -35,7 +35,7 @@ function upload_video_on_youtube($arr_data) {
     $video = new Google_Service_YouTube_Video();
   
     $videoSnippet = new Google_Service_YouTube_VideoSnippet();
-    $videoSnippet->setChannelId('PLm0GU5IUgzTCrTNRX_ijXIatqoiat8IiC');
+    $videoSnippet->setChannelId('UCt6Ed7f7MRjHf03HyVnXRsw');
     $videoSnippet->setDescription($arr_data['summary']);
     $videoSnippet->setTitle($arr_data['title']);
 
