f17772343554e9c9e965340513fae33c3674599d
vkm94 <varunmanila89@gmail.com>
2022-01-13 22:29:44 +0530

    updated

--- ConsultUs/video.php
+++ ConsultUs/video.php
@@ -72,7 +72,7 @@ include("./php/src/RtmTokenBuilder.php");
 	$user_name=$_POST['user_name'];
 	$consult_name=$_POST['consult_name'];
 	$channel_name=$user_id.$consult_id;
-                               
+               echo  $channel_name;               
 	$appID = "464ff3e49fb3409494c0956edcec52e7";
 	$appCertificate = "21f542eedcde43a38f6c292abaa8c4c2";
 	$channelName =$user_name.$consult_name;
