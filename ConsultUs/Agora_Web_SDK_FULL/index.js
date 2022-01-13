d2ad767f54294e75b174ec4b33331883adb88ea3
vkm94 <varunmanila89@gmail.com>
2022-01-14 01:06:50 +0530

    updated

--- ConsultUs/Agora_Web_SDK_FULL/index.js
+++ ConsultUs/Agora_Web_SDK_FULL/index.js
@@ -13,11 +13,11 @@ if(channel1){
 
 //alert(decodeURIComponent(id1).replace(/\+/g, '%20'));
 //alert(id1);
- var token1 =id1;
+ var token1 ="0068a0176984cea4e4e8a96c984d149d52fIAAFsM8CgbdiQVksZdvsGnuofX5EahYxdqIcfTvcrQMEEQx+f9gAAAAAEACBgxuCmMzhYQEAAQCXzOFh";
 
- var agoraAppId =appid1;
+ var agoraAppId ="8a0176984cea4e4e8a96c984d149d52f";
  console.log(agoraAppId) // Set your Agora App ID
-var channelName="hello";
+var channelName="test";
 var uid="10";
 var attend = false;
 
