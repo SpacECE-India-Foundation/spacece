e26f3ad56139fac3e9e0ce4c09ccc1e75fcffb82
Krishna <krishna.thorat20@vit.edu>
2022-01-15 01:07:18 +0530

    update

--- learnonapp/admin.php
+++ learnonapp/admin.php
@@ -4,7 +4,7 @@ include_once '../common/header_module.php';
 include_once '../common/banner.php';
 
 if (!isset($_SESSION['current_user_id']) && $_SESSION['current_user_type'] != 'admin') {
-    header("Location: ./index.php");
+    header("Location: ../spacece_auth/login.php");
 }
 ?>
 <div id="admin-page">
