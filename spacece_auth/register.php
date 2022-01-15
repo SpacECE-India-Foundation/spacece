51204fe9199932ac7bcffa41a905ae49ba149739
vkm94 <varunmanila89@gmail.com>
2022-01-15 13:59:08 +0530

    updated

--- spacece_auth/register.php
+++ spacece_auth/register.php
@@ -148,7 +148,7 @@ function get_consultant_categories($conn)
             </div>
            
                 <div class="form-group">
-                <select id="chkveg" name="chkveg">
+                <select id="chkveg" name="chkveg" multiple="multiple">
                     <option value="cheese">Cheese</option>
                     <option value="tomatoes">Tomatoes</option>
                     <option value="mozarella">Mozzarella</option>
