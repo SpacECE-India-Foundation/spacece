21c3a311d27198fcb5df5638d81ae5d25e8c0c6d
Krishna <krishna.thorat20@vit.edu>
2022-01-16 19:53:35 +0530

    update

--- learnonapp/admin.php
+++ learnonapp/admin.php
@@ -4,9 +4,11 @@ include_once '../common/header_module.php';
 include_once '../common/banner.php';
 include './placeholder.php';
 
-if (!isset($_SESSION['current_user_id']) || $_SESSION['current_user_type'] != 'admin') {
+if (!isset($_SESSION['current_user_type']) || $_SESSION['current_user_type'] != 'admin') {
     header("Location: ./index.php");
 }
+
+print_r($_SESSION);
 ?>
 <div id="admin-page">
 </div>
