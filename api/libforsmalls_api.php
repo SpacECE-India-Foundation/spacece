05af2c0e5beede1abf48364c8bf0d8e32beaf514
Krishna <krishna.thorat20@vit.edu>
2022-01-19 11:31:27 +0530

    new api

---
+++ api/libforsmalls_api.php
@@ -0,0 +1,66 @@
+<?php
+session_start();
+
+header('Access-Control-Allow-Origin: *');
+header("Access-Control-Allow-Methods: GET, OPTIONS");
+
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "khanstore";
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+?>
+
+<?php
+// [ {"bookName": "hello"},
+//  {"bookOwner" : "Sohel"}
+//  ,{"status":"available"},
+//   {"price": "133"},
+//    {"location","Delhi"}]
+
+//BookOwnerApi
+$bookname = "hello";
+$bookowner = "sohel";
+$status = "available";
+$price = "133";
+$location = "delhi";
+
+error_reporting();
+
+if (isset($_GET['action']) && $_GET['action'] == "fetch_products") {
+
+    $sql = "SELECT * FROM `products` LIMIT 9";
+    $res = $conn->query($sql);
+    header('Content-Type:application/json');
+
+    if ($res) {
+        // count that data is there or not in database
+        $count = mysqli_num_rows($res);
+        $sno = 1;
+        if ($count > 0) {
+            // we have data in database
+            while ($row = mysqli_fetch_assoc($res)) {
+
+                $arr[] = $row;   // making array of data
+
+            }
+            echo json_encode(['status' => 'success', 'data' => $arr, 'result' => 'found']);
+            //echo json_encode(['status'=>'success','result'=>'found']);
+
+
+        } else {
+            echo json_encode(['status' => 'failure', 'result' => 'not found']);
+        }
+    }
+}
+
+
+
+
+
+?>
