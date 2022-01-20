26569a4507712e27ad0b01e516e2d2d28160888b
vkm94 <varunmanila89@gmail.com>
2022-01-20 13:12:55 +0530

    updated

--- spacec_active/admin.php
+++ spacec_active/admin.php
@@ -7,6 +7,29 @@ include_once '../common/header_module.php';
 //     header("Location: ./index.php");
 // }
 ?>
+<style>
+
+ul {
+  list-style-type: none;
+  margin: 0;
+  padding: 0;
+  width: 200px;
+  background-color: #f1f1f1;
+}
+
+li a {
+  display: block;
+  color: #000;
+  padding: 8px 16px;
+  text-decoration: none;
+}
+
+/* Change the link color on hover */
+li a:hover {
+  background-color: #555;
+  color: white;
+}
+</style>
 <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" 
 integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous"/>
 <div class="main-content text-centre" style="background: linear-gradient(to bottom right, #ffcc99 10%, #ffffff 100%);">
@@ -18,11 +41,11 @@ integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg
         <li><a href="#"><i class="fas fa-users"></i> Custimers </a></li>
         <li><a href="#"><i class="far fa-bookmark"></i> Products </a></li>
         <li><a href="#"><i class="fas fa-home"></i> Categories </a></li>
-        <li><a href="#"><i class="fas fa-home"></i> Orders </a></li>
-        <li><a href="#"><i class="fas fa-home"></i> Consultants </a></li>
-        <li><a href="#"><i class="fas fa-home"></i> Appointmentd </a></li>
-        <li><a href="#"><i class="fas fa-home"></i> Profile </a></li>
-        <li><a href="#"><i class="fas fa-home"></i> Settings </a></li>
+        <li><a href="#"><i class="fas fa-cart-arrow-down"></i> Orders </a></li>
+        <li><a href="#"><i class="fas fa-headphones-alt"></i> Consultants </a></li>
+        <li><a href="#"><i class="fas fa-book"></i> Appointmentd </a></li>
+        <li><a href="#"><i class="fas fa-users"></i> Profile </a></li>
+        <li><a href="#"><i class="fas fa-cogs"></i> Settings </a></li>
 
 
 </ul>
