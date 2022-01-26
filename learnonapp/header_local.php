35c5ca3dba095cc640abc532c23fa12814ef2678
Krishna <krishna.thorat20@vit.edu>
2022-01-26 20:25:42 +0530

    update

--- learnonapp/header_local.php
+++ learnonapp/header_local.php
@@ -10,7 +10,12 @@ $extra_styles = '
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
     <link rel="stylesheet" href="./assets/css/style.css">
 ';
-$extra_scripts = "<script src='./assets/js/main.js'></script>";
+$extra_scripts = "
+<script src='https://code.jquery.com/jquery-3.3.1.slim.min.js' integrity='sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo' crossorigin='anonymous'></script>
+<script src='https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js' integrity='sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49' crossorigin='anonymous'></script>
+<script src='https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js' integrity='sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy' crossorigin='anonymous'></script>
+<script src='./assets/js/main.js'></script>
+";
 $banner_img = "../img/banner/LearnOnApp.png";
 
 if (isset($_SESSION['current_user_id'])) {
