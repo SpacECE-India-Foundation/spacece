7c68a5c3cf88c3ac32e7ce61a8c20fb7e11deb03
Krishna <krishna.thorat20@vit.edu>
2022-01-27 17:43:12 +0530

    update

--- learnonapp/assets/js/main.js
+++ learnonapp/assets/js/main.js
@@ -148,9 +148,9 @@ const addCourseSubmit = () => {
   let formData = new FormData($("#add_form_div")[0]);
   formData.append("action", "add");
 
-  // for (var pair of formData.entries()) {
-  //   console.log(pair[0] + ", " + pair[1]);
-  // }
+  for (var pair of formData.entries()) {
+    console.log(pair[0] + ", " + pair[1]);
+  }
 
   $.ajax({
     url: "../api/learnonapp_courses_add.php",
@@ -160,7 +160,6 @@ const addCourseSubmit = () => {
     contentType: false,
     success: function (d) {
       console.log(d);
-      exit();
       if (d.status == "success") {
         const courses = d.data;
         $("#admin-page").html(adminPage(courses));
