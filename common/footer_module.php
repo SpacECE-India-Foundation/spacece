7c34480eb698a0f21071663e75b27a88faaf9fd8
vkm94 <varunmanila89@gmail.com>
2022-01-31 11:19:59 +0530

    updated

--- common/footer_module.php
+++ common/footer_module.php
@@ -2,7 +2,7 @@
 <!--bug id  0000115 -->
 <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css" rel="stylesheet">
 <!-- <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script> -->
-<!-- <script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script> -->
+ <script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script> 
 
 <style>
 .fa {
