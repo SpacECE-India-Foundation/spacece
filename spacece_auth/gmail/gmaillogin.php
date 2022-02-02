920946acd78d9eb45f6801f5f70e79fa7bd7f589
vkm94 <varunmanila89@gmail.com>
2022-02-02 11:07:51 +0530

    updated

--- spacece_auth/gmail/gmaillogin.php
+++ spacece_auth/gmail/gmaillogin.php
@@ -10,11 +10,11 @@
     if(!isset($_SESSION['access_token'])) {
       //Create a URL to obtain user authorization
       $google_login_btn = '<a class="btn btn-sm btn-social" href="'.$google_client->createAuthUrl().'"><img src="https://www.tutsmake.com/wp-content/uploads/2019/12/google-login-image.png" /></a>';
-      
+      echo $google_login_btn;
      }else{
        header('Location:../index.php');
      }
-     echo $google_login_btn;
+   
   
    // $sql="Insert into social_login (email,name) VALUES('".$data['given_name']."','" .$data['email']."')";
     
