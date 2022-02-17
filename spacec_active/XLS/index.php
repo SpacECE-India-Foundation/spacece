c651965d427996efd335584f3a405603206d7859
vkm94 <varunmanila89@gmail.com>
2022-02-17 18:48:26 +0530

    updated

--- spacec_active/XLS/index.php
+++ spacec_active/XLS/index.php
@@ -5,32 +5,32 @@ include 'vendor/autoload.php';
 use PhpOffice\PhpSpreadsheet\IOFactory;
 use PhpOffice\PhpSpreadsheet\Spreadsheet;
 if(isset($_POST['submit'])){
+   
+if($_FILES["select_excel"]["name"] != '')
+{
     echo "Inside";
-// if($_FILES["select_excel"]["name"] != '')
-// {
-//     echo "Inside";
-//  $allowed_extension = array('xls', 'xlsx');
-//  $file_array = explode(".", $_FILES['select_excel']['name']);
-//  $file_extension = end($file_array);
-//  if(in_array($file_extension, $allowed_extension))
-//  {
-//   $reader = IOFactory::createReader('Xlsx');
-//   $spreadsheet = $reader->load($_FILES['select_excel']['tmp_name']);
+ $allowed_extension = array('xls', 'xlsx');
+ $file_array = explode(".", $_FILES['select_excel']['name']);
+ $file_extension = end($file_array);
+ if(in_array($file_extension, $allowed_extension))
+ {
+  $reader = IOFactory::createReader('Xlsx');
+  $spreadsheet = $reader->load($_FILES['select_excel']['tmp_name']);
 
-//   $writer = IOFactory::createWriter($spreadsheet, 'Html');
-//   $message = $writer->save('out');
-//  }
-//  else
-//  {
-//   $message = '<div class="alert alert-danger">Only .xls or .xlsx file allowed</div>';
-//  }
-// }
-// else
-// {
-//  $message = '<div class="alert alert-danger">Please Select File</div>';
-// }
+  $writer = IOFactory::createWriter($spreadsheet, 'Html');
+  $message = $writer->save('out');
+ }
+ else
+ {
+  $message = '<div class="alert alert-danger">Only .xls or .xlsx file allowed</div>';
+ }
+}
+else
+{
+ $message = '<div class="alert alert-danger">Please Select File</div>';
+}
 
-// echo $message;
+echo $message;
 
 }
 ?>
