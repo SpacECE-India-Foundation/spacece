f12511bcf0f60414fb149027db6a85c135cabd4d
vkm94 <varunmanila89@gmail.com>
2022-02-17 18:15:30 +0530

    updated

--- spacec_active/XLS/index.php
+++ spacec_active/XLS/index.php
@@ -15,8 +15,9 @@ if($_FILES["select_excel"]["name"] != '')
  {
   $reader = IOFactory::createReader('Xlsx');
   $spreadsheet = $reader->load($_FILES['select_excel']['tmp_name']);
+  
   $writer = IOFactory::createWriter($spreadsheet, 'Html');
-  $message = $writer->save('php://output');
+  $message = $writer->save('out');
  }
  else
  {
