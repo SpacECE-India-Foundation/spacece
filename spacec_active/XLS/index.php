785045e4a0b28a26068d8dd03031b83fd93622ea
vkm94 <varunmanila89@gmail.com>
2022-02-17 17:53:08 +0530

    updated

--- spacec_active/XLS/index.php
+++ spacec_active/XLS/index.php
@@ -15,7 +15,8 @@ if (isset($_POST["import"])) {
         $targetPath = 'uploads/' . $_FILES['file']['name'];
         move_uploaded_file($_FILES['file']['tmp_name'], $targetPath);
 
-        $Reader = new PhpOffice\PhpSpreadsheet\Reader\Xlsx();
+        $spreadsheet = new \PhpOffice\PhpSpreadsheet\Spreadsheet();
+
 
         $spreadSheet = $Reader->load($targetPath);
         $excelSheet = $spreadSheet->getActiveSheet();
