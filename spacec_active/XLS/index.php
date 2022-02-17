2702de23e8582214ef36b8fae136218301714360
vkm94 <varunmanila89@gmail.com>
2022-02-17 18:17:41 +0530

    updated

--- spacec_active/XLS/index.php
+++ spacec_active/XLS/index.php
@@ -8,6 +8,7 @@ if(isset($_POST['submit'])){
 
 if($_FILES["select_excel"]["name"] != '')
 {
+    echo "Inside";
  $allowed_extension = array('xls', 'xlsx');
  $file_array = explode(".", $_FILES['select_excel']['name']);
  $file_extension = end($file_array);
@@ -15,7 +16,7 @@ if($_FILES["select_excel"]["name"] != '')
  {
   $reader = IOFactory::createReader('Xlsx');
   $spreadsheet = $reader->load($_FILES['select_excel']['tmp_name']);
-  
+
   $writer = IOFactory::createWriter($spreadsheet, 'Html');
   $message = $writer->save('out');
  }
