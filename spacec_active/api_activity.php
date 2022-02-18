23a40337065fe0cc9b84589e137e029a7b083589
vkm94 <varunmanila89@gmail.com>
2022-02-18 11:08:14 +0530

    updated

--- spacec_active/api_activity.php
+++ spacec_active/api_activity.php
@@ -15,17 +15,18 @@ $query1 = mysqli_query($mysqli1, "Select * from spaceactive_activities") or die(
 
 $result = mysqli_fetch_assoc($query1);
 if($result){
-    $act_date = $result['activity_date'];
-    $act_id = $result['activity_no'];
-    $activity_name = $result['activity_name'];
-    $activity_level = $result['activity_level'];
-    $activity_dev_domain = $result['activity_dev_domain'];
-    $activity_objectives = $result['activity_objectives'];
-    $activity_key_dev = $result['activity_key_dev'];
-    $activity_material = $result['activity_material'];
-    $activity_assessment = $result['activity_assessment'];
-    $activity_process = $result['activity_process'];
-    $activity_instructions = $result['activity_instructions'];
+    echo json_encode($result);
+    // $act_date = $result['activity_date'];
+    // $act_id = $result['activity_no'];
+    // $activity_name = $result['activity_name'];
+    // $activity_level = $result['activity_level'];
+    // $activity_dev_domain = $result['activity_dev_domain'];
+    // $activity_objectives = $result['activity_objectives'];
+    // $activity_key_dev = $result['activity_key_dev'];
+    // $activity_material = $result['activity_material'];
+    // $activity_assessment = $result['activity_assessment'];
+    // $activity_process = $result['activity_process'];
+    // $activity_instructions = $result['activity_instructions'];
     
 //    $query = mysqli_query($mysqli1, "Select spaceece.users.u_id,spaceece.users.u_name,spaceece.users.u_email,spaceece.users.u_mob from spaceece.users JOIN space_active.sub_users WHERE space_active.sub_users.u_id=spaceece.users.u_id") or die('Sql Query1 Error' . mysqli_error($mysqli));
     
