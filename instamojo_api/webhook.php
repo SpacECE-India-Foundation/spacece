9dc3db62fed87005b0b8ebbb931da92d548c3a25
vkm94 <varunmanila89@gmail.com>
2022-02-23 14:49:42 +0530

    updated

--- instamojo_api/webhook.php
+++ instamojo_api/webhook.php
@@ -1,10 +1,12 @@
 <?php
+mail('varunmanila89@gmail.com',"Instamojo details","this is body");
+echo"<script>alert('sent');</script>";
 session_start();
 
 include('connect.php');
 
 $data = $_POST;
-mail('varunmanila89@gmail.com',"Instamojo details","this is body");
+
 
 
 
