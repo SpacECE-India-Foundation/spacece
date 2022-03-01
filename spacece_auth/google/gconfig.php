cae6c75d3291ef824da5aceb9938bec710e0906e
vkm94 <varunmanila@gmail.com>
2022-03-01 11:58:08 +0530

    updated

--- spacece_auth/google/gconfig.php
+++ spacece_auth/google/gconfig.php
@@ -2,7 +2,7 @@
 
 //Include Google Client Library for PHP autoload file
 require_once 'vendor/autoload.php';
-//require_once '../db.php';
+require_once '../db.php';
 //Make object of Google API Client for call Google API
 $google_client = new Google_Client();
 
