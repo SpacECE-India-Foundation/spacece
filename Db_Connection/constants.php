50ad50fe9b0b9f0f562b056d86c6412b6a7c81c0
sachin-mohite <sachin.mohite@gmail.com>
2022-04-14 12:56:31 +0100

    Update constants.php

--- Db_Connection/constants.php
+++ Db_Connection/constants.php
@@ -4,10 +4,10 @@
 //define('DB_USER_NAME', 'ostechnix');
 //define('DB_USER_PASSWORD', 'Password123#@!');  
 
-define('DB_HOST_NAME', 'spacece-database.cjnrqpvibfnn.ap-south-1.rds.amazonaws.com');
+define('DB_HOST_NAME', 'database-spacece.cjnrqpvibfnn.ap-south-1.rds.amazonaws.com');
 //define('DB_HOST_NAME', '3.109.14.4');
 define('DB_USER_NAME', 'spaceuser');
-define('DB_USER_PASSWORD', 'SpacECE12#');
+define('DB_USER_PASSWORD', 'Spaceuser12#');
 
 //  define('DB_USER_NAME', 'root');
 // define('DB_USER_PASSWORD', '');
