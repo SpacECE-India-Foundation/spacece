f7fdc27cfde31cc0d0609f3ed90ba2cdb9762c20
sachin-mohite <sachin.mohite@gmail.com>
2022-04-14 13:00:29 +0100

    Update indexDB.php

--- Db_Connection/indexDB.php
+++ Db_Connection/indexDB.php
@@ -1,12 +1,8 @@
 <?php
 //session_start();
+include('constants.php');
+
 define("SITEURL", '');
- $servername = "localhost";
-// $username = "root";
-// $password = "";
-//$servername = "3.109.14.4";
-$username = "ostechnix";
-$password = "Password123#@!";
 $dbname = "consultant_app";
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
