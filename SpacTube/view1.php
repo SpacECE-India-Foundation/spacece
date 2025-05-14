d04d7a4917741313dee779064e53b53e1ddd791b
harini <b.harini@bcah.christuniversity.in>
2025-05-14 11:06:40 +0530

    harini_aboutus_trendingpage_finished

--- SpacTube/view1.php
+++ SpacTube/view1.php
@@ -5,7 +5,7 @@ $Fun_call = new Functions();
 
 include_once './header_local.php';
 include_once '../common/header_module.php';
-include_once '../common/banner.php';
+
 
 $status = 'false';
 if (isset($_SESSION['current_user_email'])) {
@@ -32,24 +32,15 @@ $get_video = $Fun_call->selected_order('videos', 'filter');
             <a href="user1.php">
                 <button type="button">Remove Video</button>
             </a> -->
-</div>
-<div class="container">
+</div> 
+<div class="container-fluid">
+<body style="background-color: #eeeeee;">
+<div class="container" style="margin-top: 20px;">
             <?php include 'menu.php'; ?>
         </div>
-<?php
-
+<?php include_once '../common/banner.php';
 if ($status === true) {
-
-
 ?>
-    <div class="container-fluid">
-
-      
-
-
-
-
-
         <div class="container">
             <div class="ins-box">
                 <div class="container">
@@ -217,5 +208,5 @@ if ($status === true) {
 <script src="https://code.jquery.com/jquery-3.4.1.min.js" integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo=" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
-
+</body>
 <?php include_once '../common/footer_module.php'; ?>
