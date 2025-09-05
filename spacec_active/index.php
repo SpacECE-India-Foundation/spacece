218c71ff0849c97f415f7eabde6f107db0bc1967
Tej Pratap Singh <162354068+tej292@users.noreply.github.com>
2025-09-05 14:43:46 +0530

    add onclick functionality to the button and give a registration file path "add_activity.php"
    0000494: 'In-Person Visit Registration' button is not functional in the 'Spacactive' page with delivery boy login.
                          add onclick functionality to the button and give a registration file path "add_activity.php"' and button is perform clearliy
                          issue is resolved now

--- spacec_active/index.php
+++ spacec_active/index.php
@@ -283,11 +283,11 @@ table#activity_table td {
                         Online Activity Registration
                     </button>
                     <!-- 0000494: 'In-Person Visit Registration' button is not functional in the 'Spacactive' page with delivery boy login.
-                      add onclick functionality to the button and give a registration file path "add_space_activity.php"' and button is perform clearliy
+                      add onclick functionality to the button and give a registration file path "add_activity.php"' and button is perform clearliy
                       issue is resolved now
                     -->
                     
-                    <button type="button" onclick="window.location.href='add_space_activity.php'" class="btn" style="background-color: #007BFF; color: white; padding: 10px 20px; border-radius: 8px; border: none; margin-right: 4px;">
+                    <button type="button" onclick="window.location.href='add_activity.php'" class="btn" style="background-color: #007BFF; color: white; padding: 10px 20px; border-radius: 8px; border: none; margin-right: 4px;">
                         In-Person Visit Registration
                     </button>
                 </div>
