6bf21bc8d719509710f7b3a09c0bd3e153cb5264
avaninakrani5-sys <avaninakrani5@gmail.com>
2025-10-15 11:55:15 +0530

    0000558:'Your Appointments' button is  functional
    Redirects the user to the 'appointment-history' page

--- child_profile/immunization.php
+++ child_profile/immunization.php
@@ -141,7 +141,13 @@ session_start();
           window.location.href = 'book-appointment.php'; // correct page
         }
       </script>
-      <a href="cits/appointment-history.php"><button class="appointment-btn">Your Appointments</button></a>
+      <!--  Bug Number 0000558 : Redirects the user to the 'appointment-history' page -->
+      <button class="book-btn"  onclick="redirectToAppointments()">Your Appointments</button>
+      <script>
+        function redirectToAppointments() {
+          window.location.href = 'appointment-history.php'; // correct page
+        }
+      </script>
     </div>
 
     <table>
